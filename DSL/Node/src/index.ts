eeefc398b12b59918b4597773fdc59cae85b03c6
Bahaa Alsharif <bhlshrf@gmail.com>
2023-03-01 17:56:50 +0300

    35 Ruuter service to create resql services (#2)
    * Add DSL endpoints for adding new Resqls
    
    * Add an empty line
    
    * Add nodejs file service
    
    * Add empty lines at the end of files
    
    * Change node service volume path
    
    * Fix a typo and use single quotes for imports
    
    * Change indent from 4 to 2
    
    * Revert `tsconfig.json` changes
    
    * Fix a typo

---
+++ DSL/Node/src/index.ts
@@ -0,0 +1,15 @@
+import express, { Express } from 'express';
+import 'dotenv/config';
+import file from './file';
+
+const app: Express = express();
+const port = process.env.PORT || 3003
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+app.use('/file', file);
+app.get('/status', (req, res) => res.status(200).send('ok'))
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
