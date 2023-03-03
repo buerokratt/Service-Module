76d11edf0e36c1d697d154519fcc78bb89660e6b
Bahaa Alsharif <bhlshrf@gmail.com>
2023-03-03 10:32:15 +0300

    38 rest service services to fetch a list of all services (#3)
    * Add DSL endpoints for adding new Resqls
    
    * Add an empty line
    
    * Add nodejs file service
    
    * Add empty lines at the end of files
    
    * Change node service volume path
    
    * Fix a typo and use single quotes for imports
    
    * Change indent from 4 to 2
    
    * Revert `tsconfig.json` changes
    
    * Fix a typo
    
    * Fetch Ruuter services
    
    * Rename node service

--- DSL/Node/src/index.ts
+++ DSL/Node/src/index.ts
@@ -1,6 +1,7 @@
 import express, { Express } from 'express';
 import 'dotenv/config';
 import file from './file';
+import ruuter from './ruuter';
 
 const app: Express = express();
 const port = process.env.PORT || 3003
@@ -8,6 +9,7 @@ const port = process.env.PORT || 3003
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 app.use('/file', file);
+app.use('/ruuter', ruuter);
 app.get('/status', (req, res) => res.status(200).send('ok'))
 
 app.listen(port, () => {
