5cef1d45c5b650e04ae59df99ef729c91dc6e4f9
ahmedyasser <ahmedyasser.dev@hotmail.com>
2023-03-09 00:05:01 +0200

    - Modified Changeset - Modified Set Status

--- DSL/Liquibase/changelog/1678232509-add-ruuter-types.sql
+++
@@ -1,9 +0,0 @@
--- liquibase formatted sql
--- changeset ahmed:1678232509
-CREATE TYPE ruuter_api_types AS ENUM (
- 'GET',
- 'POST'
-);
-
-ALTER TABLE services 
-ADD COLUMN ruuter_type ruuter_api_types DEFAULT 'GET';
