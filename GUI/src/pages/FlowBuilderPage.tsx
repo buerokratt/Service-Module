b92c6a6f3ebb1852de4fe9201510b0768e587a69
baha-a <bhlshrf@gmail.com>
2023-03-08 15:53:34 +0300

    Add condition builder

--- GUI/src/pages/FlowBuilderPage.tsx
+++ GUI/src/pages/FlowBuilderPage.tsx
@@ -1,20 +1,28 @@
 import React, { useEffect, useState } from 'react'
+import { DndProvider } from 'react-dnd'
+import { HTML5Backend } from 'react-dnd-html5-backend'
 import { useTranslation } from 'react-i18next'
 import { Track } from '../components'
+import FlowCanvas, { StepTypes } from '../components/FlowCanvas'
 
 const FlowBuilderPage: React.FC = () => {
   return (
-    <Track direction='horizontal' justify='between' align='left'>
-      <Track direction='vertical'>
-        <h4>Request and response elements</h4>
+    <DndProvider backend={HTML5Backend}>
+      <Track direction='horizontal' justify='between' align='left'>
+        <Track direction='vertical'>
+          <h4>Request and response elements</h4>
+        </Track>
+        <Track direction='vertical'>
+          <h4>Ordering the service flow</h4>
+          <FlowCanvas />
+        </Track>
+        <Track direction='vertical'>
+          <h4>Elements used in the Service</h4>
+          <StepTypes />
+        </Track>
       </Track>
-      <Track direction='vertical'>
-        <h4>Ordering the service flow</h4>
-      </Track>
-      <Track direction='vertical'>
-        <h4>Elements used in the Service</h4>
-      </Track>
-    </Track>
+    </DndProvider>
+
   )
 }
 
---
+++ GUI/src/store/actions/steps.ts
@@ -0,0 +1,52 @@
+import { ReduxAction } from '../../types/reducers'
+import { DropType } from '../../types/reducers'
+
+export enum STEP_ACTIONS {
+    ADD_STEP = 'steps/add',
+    DELETE = 'steps/delete',
+    CHANGE_TITLE = 'steps/title',
+    CHANGE_TEXT_CONTENT = 'steps/change/text',
+    ADD_CONDITION = 'steps/condition/add',
+    DELETE_CONDITION = 'steps/condition/delete',
+
+    CHANGE_ORDER = 'steps/order',
+}
+
+export const addStepAction: ReduxAction<{ stepType: DropType, conditionId?: number }>
+    = ({ stepType, conditionId = undefined }) => {
+        return {
+            type: STEP_ACTIONS.ADD_STEP,
+            payload: {
+                stepType,
+                conditionId,
+            }
+        }
+    }
+
+export const deleteItemAction: ReduxAction<number> = (id: number) => {
+    return {
+        type: STEP_ACTIONS.DELETE,
+        payload: id,
+    }
+}
+
+export const changeStepTitleAction: ReduxAction<{ id: number, title: string }> = (payload) => {
+    return {
+        type: STEP_ACTIONS.CHANGE_TITLE,
+        payload,
+    }
+}
+
+export const changeTextStepContentAction: ReduxAction<{ id: number, text: string }> = (payload) => {
+    return {
+        type: STEP_ACTIONS.CHANGE_TEXT_CONTENT,
+        payload,
+    }
+}
+
+export const addConditionAction: ReduxAction<number> = (parentId) => {
+    return {
+        type: STEP_ACTIONS.ADD_CONDITION,
+        payload: parentId,
+    }
+}
