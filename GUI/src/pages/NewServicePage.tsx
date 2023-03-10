21a8bcc410d01540a619edc0bebe8bde6a81b1b3
ahmedyasser <ahmedyasser.dev@hotmail.com>
2023-03-10 10:08:13 +0200

    - Added Endpoints Decode

--- GUI/src/pages/NewServicePage.tsx
+++ GUI/src/pages/NewServicePage.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 import axios from "axios";
-import { Button, FormInput, Track } from "../components";
+import { Button, FormInput, FormSelect, Track } from "../components";
 import { openApiSpeckMock } from "../resources/api-constants";
 import toJsonSchema, {
   fromParameter,
@@ -9,16 +9,28 @@ import toJsonSchema, {
 import Form from "@rjsf/core";
 import { RJSFSchema } from "@rjsf/utils";
 import { openApiToJsonSchema } from "openapi-json-schema";
+import { EndpointType } from "../types/endpointType";
 
 const NewServicePage: React.FC = () => {
   const [openApiSpec, setOpenApiSpec] = useState("");
-  const [jsonSchema, setJsonSchema] = useState<Record<string | number, any>>();
+  const [jsonSchema, setJsonSchema] = useState<any>();
+  const [endpoints, setEndpoints] = useState<EndpointType[]>([]);
+  const [selectedEndpoint, setSelectedEndpoint] = useState<string>("");
 
   const { t } = useTranslation();
 
   useEffect(() => {
-    // fetchOpenApiSpecMock();
-  }, []);
+    if (selectedEndpoint.length > 0) {
+      const parameters = jsonSchema.paths[selectedEndpoint].post.parameters;
+      if (parameters != undefined) {
+        parameters.forEach((e: Record<string | number, any>) => {
+          var convertedSchema = fromParameter(e);
+          // console.log(convertedSchema);
+        });
+      }
+      console.log(jsonSchema.paths[selectedEndpoint].post.parameters);
+    }
+  }, [selectedEndpoint]);
 
   const fetchOpenApiSpecMock = async () => {
     const result = await axios.post(openApiSpeckMock());
@@ -33,29 +45,43 @@ const NewServicePage: React.FC = () => {
     //   },
     // };
     // var convertedSchema = fromParameter(param);
-    const convertedSchema = openApiToJsonSchema(result.data.response);
-    console.log(convertedSchema);
+    const convertedSchema = toJsonSchema(result.data.response);
+    const paths = Object.keys(convertedSchema.paths);
+    const endpointsArr: EndpointType[] = [];
+    paths.forEach((e) => {
+      endpointsArr.push({
+        label: `${e}`,
+        value: `${e}`,
+      });
+    });
+    setEndpoints(endpointsArr);
     setJsonSchema(convertedSchema);
     setOpenApiSpec(result.data.response);
   };
 
   return (
     <Track direction="vertical">
-      <link
-        rel="stylesheet"
-        href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
-      ></link>
       <h1>{t("menu.newService")}</h1>
+      <a style={{ marginBottom: "10px" }}></a>
       <Track>
         <FormInput name={""} label={""}></FormInput>
+        <a style={{ marginLeft: "20px" }}></a>
         <Button>Ask For Spec</Button>
+        <a style={{ marginLeft: "10px" }}></a>
         <Button appearance="text" onClick={fetchOpenApiSpecMock}>
           Mock It
         </Button>
       </Track>
-      {/* <Form schema={jsonSchema} validator={validator} /> */}
-      {/* <span>{JSON.stringify(openApiSpec, null, 2)}</span> */}
-      <span>{JSON.stringify(jsonSchema, null, 2)}</span>
+      <a style={{ marginBottom: "20px" }}></a>
+      {endpoints.length > 0 && (
+        <FormSelect
+          name={""}
+          label={""}
+          placeholder={"Select Endpoint"}
+          options={endpoints}
+          onSelectionChange={(value) => setSelectedEndpoint(value?.value ?? "")}
+        />
+      )}
     </Track>
   );
 };
