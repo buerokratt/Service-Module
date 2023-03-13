494ad30a74c1c7268fbb1cb0aa7f241daf5185e5
trevorling <53426191+trevorling@users.noreply.github.com>
2023-03-13 11:00:32 +0200

    Change query: "is-user-roles-allowed"

--- DSL/Resql/services/is-user-roles-allowed.sql
+++ DSL/Resql/services/is-user-roles-allowed.sql
@@ -1,9 +1,17 @@
-SELECT ARRAY[(
-    SELECT array_agg(authorities)::text[] AS "user_roles"
-    FROM (
-        SELECT unnest(authority_name) AS authorities
-        FROM public.user_authority
-        WHERE user_id = :userId::text
-        ORDER BY authorities
-      ) AS _
-  )] @> ARRAY[(:allowedRoles)]::text[] as "is_allowed";
+SELECT (
+        SELECT array_agg(authorities)::text[] AS "user_roles"
+        FROM (
+                SELECT unnest(authority_name) AS authorities
+                FROM public.user_authority
+                WHERE user_id = :userId::text
+                ORDER BY authorities
+            ) AS _
+    ) @> (
+        SELECT (
+                SELECT array_agg(allowedRole)::text[] AS "roles"
+                FROM(
+                        SELECT unnest(ARRAY [:allowedRoles::text]::text[]) AS allowedRole
+                        ORDER BY allowedRole
+                    ) as __
+            )
+    ) AS "is_allowed";
