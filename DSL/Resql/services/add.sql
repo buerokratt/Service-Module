0c90fda2d229b6c68dc302b00776c2904a13c99a
ahmedyasser <ayasser@softmills.com>
2023-03-13 04:10:06 +0200

    - Added .toUpperCase

--- DSL/Resql/services/add.sql
+++ DSL/Resql/services/add.sql
@@ -1,2 +1,2 @@
 INSERT INTO services (name, description,ruuter_type)
-VALUES (:name, :description,UPPER(:ruuter_type)::ruuter_request_type);
+VALUES (:name, :description,:ruuter_type::ruuter_request_type);
