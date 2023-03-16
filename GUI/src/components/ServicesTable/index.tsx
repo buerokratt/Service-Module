7748c916de98203732fc1902b7977416eed902e2
Mihhail Kohhantsuk <mihhail@devtailor.com>
2023-03-16 14:50:08 +0200

    Create services table with dummy data

---
+++ GUI/src/components/ServicesTable/index.tsx
@@ -0,0 +1,102 @@
+import { createColumnHelper, PaginationState } from "@tanstack/react-table";
+import { useEffect, useState } from "react";
+import { useTranslation } from "react-i18next";
+import { MdDeleteOutline, MdOutlineEdit } from "react-icons/md";
+import { Button, Card, Icon, Label, Track } from "..";
+import { Service } from "../../types/service";
+import DataTable from "../DataTable";
+
+import "./ServicesTable.scss";
+
+type Props = {
+  dataSource: Service[];
+};
+
+const ServicesTable = (props: Props) => {
+  const [pagination, setPagination] = useState<PaginationState>({
+    pageIndex: 0,
+    pageSize: 10,
+  });
+  const { t } = useTranslation();
+  const [services, setServices] = useState<Service[]>([]);
+  const columnHelper = createColumnHelper<Service>();
+
+  useEffect(() => {
+    const fetchServices = async () => setServices(props.dataSource);
+
+    fetchServices().catch(console.error);
+  }, [props.dataSource]);
+
+  const columns = [
+    columnHelper.accessor("name", {
+      header: t("overview.service.name") ?? "",
+      meta: {
+        size: 530,
+      },
+    }),
+    columnHelper.accessor("usedCount", {
+      header: t("overview.service.usedCount") ?? "",
+      meta: {
+        size: 320,
+      },
+    }),
+    columnHelper.accessor("state", {
+      header: t("overview.service.state") ?? "",
+      meta: {
+        size: 120,
+      },
+      cell: (props) => (
+        <Track justify="around">
+          <Label
+            type={props.row.original.state === "active" ? "success" : "error"}
+            tooltip={<></>}
+          >
+            {t(`overview.service.status.${props.row.original.state}`)}
+          </Label>
+        </Track>
+      ),
+    }),
+    columnHelper.display({
+      id: "edit",
+      meta: {
+        size: 90,
+      },
+      cell: (_) => (
+        <Track align="right" justify="start">
+          <Button appearance="text">
+            <Icon icon={<MdOutlineEdit />} size="medium" />
+            {t("overview.edit")}
+          </Button>
+        </Track>
+      ),
+    }),
+    columnHelper.display({
+      id: "remove",
+      meta: {
+        size: 90,
+      },
+      cell: (_) => (
+        <Track align="right">
+          <Button appearance="text">
+            <Icon icon={<MdDeleteOutline />} size="medium" />
+            {t("overview.delete")}
+          </Button>
+        </Track>
+      ),
+    }),
+  ];
+
+  return (
+    <Card>
+      <DataTable
+        sortable={true}
+        data={services}
+        columns={columns}
+        pagination={pagination}
+        setPagination={setPagination}
+      />
+    </Card>
+  );
+};
+
+export default ServicesTable;
--- GUI/src/resources/api-constants.ts
+++ GUI/src/resources/api-constants.ts
@@ -1,8 +1,22 @@
-const baseUrl = process.env.REACT_APP_API_URL
+const baseUrl = process.env.REACT_APP_API_URL;
 
 export const dummyDataApi = (): string => {
-  return baseUrl + '/testing'
-}
+  return baseUrl + "/testing";
+};
 
 // Services
 export const getServicesAdd = (): string => `${baseUrl}/services/add`;
+
+export const dummyServiceData = [
+  { name: "first", usedCount: 1, state: "inactive" },
+  { name: "second", usedCount: 20, state: "active" },
+  { name: "third", usedCount: 3000, state: "inactive" },
+  { name: "fourth", usedCount: 4000, state: "active" },
+  { name: "fifth", usedCount: 500000, state: "active" },
+  { name: "sixth", usedCount: 6000000, state: "active" },
+  { name: "seventh", usedCount: 70000000, state: "active" },
+  { name: "eight", usedCount: 800000000, state: "active" },
+  { name: "ninth", usedCount: 900000000, state: "active" },
+  { name: "tenth", usedCount: 10000000000, state: "active" },
+  { name: "eleventh", usedCount: 11, state: "inactive" },
+];
