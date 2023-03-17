d7dfdccd02d73ddb73a66a6acdb38e48b0128127
ahmedyasser <ayasser@softmills.com>
2023-03-17 04:44:55 +0200

    - Modified Open Search

--- DSL/OpenSearch/init.sh
+++ DSL/OpenSearch/init.sh
@@ -1,6 +1,6 @@
 #clear
-curl -XDELETE 'http://localhost:9200/*' -u admin:admin --insecure
+curl -XDELETE 'https://localhost:9200/*' -u admin:admin --insecure
 
 #intents
-curl -H "Content-Type: application/x-ndjson" -X PUT "http://localhost:9200/intents" -ku admin:admin --data-binary "@fieldMappings/intents.json"
-curl -H "Content-Type: application/x-ndjson" -X PUT "http://localhost:9200/intents/_bulk" -ku admin:admin --data-binary "@mock/intents.json"
+curl -H "Content-Type: application/x-ndjson" -X PUT "https://localhost:9200/intents" -ku admin:admin --data-binary "@fieldMappings/intents.json"
+curl -H "Content-Type: application/x-ndjson" -X PUT "https://localhost:9200/intents/_bulk" -ku admin:admin --data-binary "@mock/intents.json"
