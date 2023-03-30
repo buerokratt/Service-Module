5391f7688c7b4ad287a85072e242ebf20aade635
Mihhail Kohhantsuk <mihhail@devtailor.com>
2023-03-30 16:07:40 +0300

    Fix step counter import

--- GUI/src/components/NewServiceHeader/index.tsx
+++ GUI/src/components/NewServiceHeader/index.tsx
@@ -1,6 +1,6 @@
 import { t } from "i18next";
 import React, { FC } from "react";
-import { Button, StepCounter, Track } from "..";
+import { Button, HeaderStepCounter, Track } from "..";
 import "../Header/Header.scss";
 
 
@@ -14,7 +14,7 @@ const NewServiceHeader: FC<NewServiceHeaderProps> = ({activeStep}) => {
       <header className="header" style={{ paddingLeft: 24 }}>
         <Track justify="between" gap={16}>
           <h1 style={{ whiteSpace: "nowrap" }}>{t("menu.newService")}</h1>
-          <StepCounter activeStep={activeStep} />
+          <HeaderStepCounter activeStep={activeStep} />
           <Button appearance="secondary">{t("newService.saveDraft")}</Button>
           <Button>{t("global.continue")}</Button>
         </Track>
