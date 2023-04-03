74b706a2622a73fec7dfb105121c13a512b78a07
ahmedyasser <ayasser@softmills.com>
2023-04-03 10:40:34 +0200

    Changes
    Added Changes to open search init

--- DSL/OpenSearch/init.sh
+++ DSL/OpenSearch/init.sh
@@ -5,3 +5,4 @@ curl -XDELETE 'http://localhost:9200/*' -u admin:admin --insecure
 curl -H "Content-Type: application/x-ndjson" -X PUT "http://localhost:9200/intents" -ku admin:admin --data-binary "@fieldMappings/intents.json"
 curl -H "Content-Type: application/x-ndjson" -X PUT "http://localhost:9200/intents/_bulk" -ku admin:admin --data-binary "@mock/intents.json"
 curl -L -X POST 'http://localhost:9200/_scripts/search_intents' -H 'Content-Type: application/json' --data-binary "@templates/search_intents.json"
+
