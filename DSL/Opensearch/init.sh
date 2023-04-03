f06790d5ddf8dedd7c9619b33f96d311abe17ab4
ahmedyasser <ayasser@softmills.com>
2023-04-03 10:32:47 +0200

    - Merged with main

--- DSL/Opensearch/init.sh
+++ DSL/Opensearch/init.sh
@@ -5,3 +5,8 @@ curl -XDELETE 'http://localhost:9200/*' -u admin:admin --insecure
 curl -H "Content-Type: application/x-ndjson" -X PUT "http://localhost:9200/services" -ku admin:admin --data-binary "@fieldMappings/services.json"
 curl -H "Content-Type: application/x-ndjson" -X PUT "http://localhost:9200/services/_bulk" -ku admin:admin --data-binary "@mock/services.json"
 curl -L -X POST 'http://localhost:9200/_scripts/get-services-stat' -H 'Content-Type: application/json' -H 'Cookie: customJwtCookie=test' --data-binary "@templates/get-services-stat.json"
+
+#intents
+curl -H "Content-Type: application/x-ndjson" -X PUT "http://localhost:9200/intents" -ku admin:admin --data-binary "@fieldMappings/intents.json"
+curl -H "Content-Type: application/x-ndjson" -X PUT "http://localhost:9200/intents/_bulk" -ku admin:admin --data-binary "@mock/intents.json"
+curl -L -X POST 'http://localhost:9200/_scripts/search_intents' -H 'Content-Type: application/json' --data-binary "@templates/search_intents.json"
