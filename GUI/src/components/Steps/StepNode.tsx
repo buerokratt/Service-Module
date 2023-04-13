b9fb75a754923994094a9fa8ce08276b41467830
trevorling <53426191+trevorling@users.noreply.github.com>
2023-04-13 14:32:22 +0300

    StepNode: show ExlamationBadge

--- GUI/src/components/Steps/StepNode.tsx
+++ GUI/src/components/Steps/StepNode.tsx
@@ -1,27 +1,35 @@
 import { FC, memo } from 'react';
-import { useTranslation } from 'react-i18next';
 
 import { ExclamationBadge, Track } from '../';
+import { StepType } from '../../types/step';
 
 type NodeDataProps = {
   data: {
     label: string;
     onDelete: (id: string) => void;
+    onEdit: (id: string) => void;
     type: string;
+    stepType: StepType;
     readonly: boolean;
+    message?: string;
   }
 }
 
 const StepNode: FC<NodeDataProps> = ({ data }) => {
-  const { t } = useTranslation();
+
+  const createMarkup = (text: string) => {
+    return {
+      __html: text
+    };
+  };
 
   return (
     <Track style={{ width: '100%' }} direction='vertical' align='left'>
       <p>
-        {!data.readonly && <ExclamationBadge></ExclamationBadge>}
+        {!(data.readonly || !!data.message?.length) && <ExclamationBadge></ExclamationBadge>}
         {data.label}
       </p>
-      <p>Something something</p>
+      <div dangerouslySetInnerHTML={createMarkup(data.message ?? '')}></div>
     </Track>
   );
 };
