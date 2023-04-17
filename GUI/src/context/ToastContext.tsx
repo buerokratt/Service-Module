56758b7feb867b2f97846e14f83deaf866059a0d
ahmedyasser <ayasser@softmills.com>
2023-04-17 10:20:14 +0200

    Addressed Comments

--- GUI/src/context/ToastContext.tsx
+++ GUI/src/context/ToastContext.tsx
@@ -10,7 +10,7 @@ import { useTranslation } from 'react-i18next';
 import * as RadixToast from '@radix-ui/react-toast';
 
 import { Toast } from '../components';
-import { generateUEID } from '../utils/generateUEID';
+import { v4 as uuid } from 'uuid'
 
 export type ToastType = {
   type: 'info' | 'success' | 'error' | 'warning';
@@ -19,6 +19,8 @@ export type ToastType = {
 };
 
 type ToastTypeWithId = ToastType & { id: string };
+
+const id: string = uuid();
 
 type ToastContextType = {
   open: (toast: ToastType) => void;
@@ -32,7 +34,7 @@ export const ToastProvider: FC<PropsWithChildren> = ({ children }) => {
   const open = (content: ToastType) => {
     setToasts((prevState) => [
       ...prevState,
-      { id: generateUEID(), ...content },
+      { id: id, ...content },
     ]);
   };
   const close = (id: string) => {
