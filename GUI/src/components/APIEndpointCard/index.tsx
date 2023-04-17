3f5578f7298e679cf25a89b04309acc38a392e77
Mihhail Kohhantsuk <mihhail@devtailor.com>
2023-04-17 17:35:25 +0300

    cleanup

--- GUI/src/components/APIEndpointCard/index.tsx
+++ GUI/src/components/APIEndpointCard/index.tsx
@@ -15,14 +15,20 @@ import { Option } from "../../types/option";
 import { useTranslation } from "react-i18next";
 import { MdDeleteOutline } from "react-icons/md";
 import "./APIEndpointCard.scss";
+
 type EndpointCardProps = {
   onDelete: () => void;
 };
 
+enum EndpointEnv {
+  Live = "live",
+  Test = "test",
+}
+
 const APIEndpointCard: FC<EndpointCardProps> = ({ onDelete }) => {
   const [option, setOption] = useState<Option | null>();
-  const [selectedTab, setSelectedTab] = useState<string>("live");
-  const [endpointName, setEndpointName] = useState<string>();
+  const [selectedTab, setSelectedTab] = useState<EndpointEnv>(EndpointEnv.Live);
+  const [endpointName, setEndpointName] = useState<string>("");
   const [testEnvExists, setTestEnvExists] = useState<boolean>(false);
   const options = [
     { label: "Open API", value: "openAPI", name: "da" },
@@ -31,24 +37,30 @@ const APIEndpointCard: FC<EndpointCardProps> = ({ onDelete }) => {
   ];
   const { t } = useTranslation();
 
-  const getTabTriggerClasses = (tab: string) =>
+  const getTabTriggerClasses = (tab: EndpointEnv) =>
     `tab-group__tab-btn ${selectedTab === tab ? "active" : ""}`;
 
   return (
     <Tabs.Root
-      defaultValue="live"
+      defaultValue={EndpointEnv.Live}
       onValueChange={(value) => {
-        setSelectedTab(value);
-        if (value === "test") setTestEnvExists(true);
+        setSelectedTab(value as EndpointEnv);
+        if (value === EndpointEnv.Test) setTestEnvExists(true);
       }}
       className="tab-group"
     >
       <Track justify="between">
         <Tabs.List className="tab-group__list" aria-label="environment">
-          <Tabs.Trigger className={getTabTriggerClasses("live")} value="live">
+          <Tabs.Trigger
+            className={getTabTriggerClasses(EndpointEnv.Live)}
+            value={EndpointEnv.Live}
+          >
             {t("newService.endpoint.live")}
           </Tabs.Trigger>
-          <Tabs.Trigger className={getTabTriggerClasses("test")} value="test">
+          <Tabs.Trigger
+            className={getTabTriggerClasses(EndpointEnv.Test)}
+            value={EndpointEnv.Test}
+          >
             {t(
               testEnvExists
                 ? "newService.endpoint.testEnv"
@@ -67,68 +79,47 @@ const APIEndpointCard: FC<EndpointCardProps> = ({ onDelete }) => {
           </Button>
         </>
       </Track>
-      <Tabs.Content className="tab-group__tab-content" value="live">
-        <Track direction="vertical" align="stretch" gap={16}>
-          <div>
-            <label htmlFor="service-type">{t("newService.uses")}</label>
-            <FormSelect
-              name="service-type"
-              label={""}
-              options={options}
-              placeholder={t("global.choose") ?? ""}
-              onSelectionChange={(selection) => setOption(selection)}
-              defaultValue={option?.value}
-            />
-          </div>
-          {option && (
-            <div>
-              <label htmlFor="endpointName">
-                {t("newService.endpoint.name")}
-              </label>
-              <FormInput
-                name="endpointName"
-                label=""
-                value={endpointName}
-                onChange={(e) => setEndpointName(e.target.value)}
-              />
-            </div>
-          )}
-          {option?.value === "openAPI" && <EndpointOpenAPI />}
-          {option?.value === "custom" && <EndpointCustom />}
-          {option?.value === "resql" && <EndpointResqlComponent />}
-        </Track>
-      </Tabs.Content>
-      <Tabs.Content className="tab-group__tab-content" value="test">
-        <Track direction="vertical" align="stretch" gap={16}>
-          <div>
-            <label htmlFor="service-type">{t("newService.uses")}</label>
-            <FormSelect
-              name="service-type"
-              label={""}
-              disabled
-              placeholder={t("global.choose") ?? ""}
-              options={options}
-              defaultValue={option?.value}
-            />
-          </div>
-          {option && (
-            <div>
-              <label htmlFor="endpointName">
-                {t("newService.endpoint.name")}
-              </label>
-              <FormInput
-                name="endpointName"
-                label=""
-                value={endpointName}
-                disabled
-              />
-            </div>
-          )}
-          {option?.value === "openAPI" && <EndpointOpenAPI />}
-          {option?.value === "custom" && <EndpointCustom />}
-          {option?.value === "resql" && <EndpointResqlComponent />}
-        </Track>
-      </Tabs.Content>
+      {[EndpointEnv.Live, EndpointEnv.Test].map((env) => {
+        return (
+          <Tabs.Content
+            className="tab-group__tab-content"
+            value={env}
+            key={env}
+          >
+            <Track direction="vertical" align="stretch" gap={16}>
+              <div>
+                <label htmlFor="service-type">{t("newService.uses")}</label>
+                <FormSelect
+                  name="service-type"
+                  label={""}
+                  options={options}
+                  disabled={selectedTab === EndpointEnv.Test}
+                  placeholder={t("global.choose") ?? ""}
+                  onSelectionChange={(selection) => setOption(selection)}
+                  defaultValue={option?.value}
+                />
+              </div>
+              {option && (
+                <div>
+                  <label htmlFor="endpointName">
+                    {t("newService.endpoint.name")}
+                  </label>
+                  <FormInput
+                    name="endpointName"
+                    label=""
+                    value={endpointName}
+                    disabled={selectedTab === EndpointEnv.Test}
+                    onChange={(e) => setEndpointName(e.target.value)}
+                  />
+                </div>
+              )}
+              {option?.value === "openAPI" && <EndpointOpenAPI />}
+              {option?.value === "custom" && <EndpointCustom />}
+              {option?.value === "resql" && <EndpointResqlComponent />}
+            </Track>
+          </Tabs.Content>
+        );
+      })}
     </Tabs.Root>
   );
 };
