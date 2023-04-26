f79b4b4037713a141c2b083ae0baaa117c939d80
baha-a <bhlshrf@gmail.com>
2023-04-26 09:31:46 +0300

    Add request logs to opensearch

--- DSL/OpenSearch/init.sh
+++ DSL/OpenSearch/init.sh
@@ -10,3 +10,9 @@ curl -L -X POST 'http://localhost:9200/_scripts/search_intents' -H 'Content-Type
 curl -H "Content-Type: application/x-ndjson" -X PUT "http://localhost:9200/services" -ku admin:admin --data-binary "@fieldMappings/services.json"
 curl -H "Content-Type: application/x-ndjson" -X PUT "http://localhost:9200/services/_bulk" -ku admin:admin --data-binary "@mock/services.json"
 curl -L -X POST 'http://localhost:9200/_scripts/get-services-stat' -H 'Content-Type: application/json' -H 'Cookie: customJwtCookie=test' --data-binary "@templates/get-services-stat.json"
+
+#logs
+curl -H "Content-Type: application/x-ndjson" -X PUT "http://localhost:9200/logs" -ku admin:admin --data-binary "@fieldMappings/logs.json"
+curl -H "Content-Type: application/x-ndjson" -X PUT "http://localhost:9200/logs/_bulk" -ku admin:admin --data-binary "@mock/logs.json"
+curl -L -X POST 'http://localhost:9200/_scripts/get-log-by-request-id' -H 'Content-Type: application/json' -H 'Cookie: customJwtCookie=test' --data-binary "@templates/get-log-by-request-id.json"
+curl -L -X POST 'http://localhost:9200/_scripts/get-log-by-level' -H 'Content-Type: application/json' -H 'Cookie: customJwtCookie=test' --data-binary "@templates/get-log-by-level.json"
