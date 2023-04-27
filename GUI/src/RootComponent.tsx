23e4f265133be39d662416fba8deff7ddbc4902b
baha-a <bhlshrf@gmail.com>
2023-04-27 10:18:43 +0300

    Fix routes order

--- GUI/src/RootComponent.tsx
+++ GUI/src/RootComponent.tsx
@@ -13,11 +13,11 @@ import './styles/main.scss'
 const RootComponent: React.FC = () => {
   return (
     <Routes>
-      <Route path={ROUTES.NEWSERVICE_ROUTE + "/old"} element={<OldNewServicePage />} />
       <Route path={ROUTES.NEWSERVICE_ROUTE} element={<NewServicePage />} />
+      <Route path={ROUTES.FLOW_ROUTE} element={<ServiceFlowPage />} />
       <Route element={<Layout />}>
+        <Route path={ROUTES.NEWSERVICE_ROUTE + "/old"} element={<OldNewServicePage />} />
         <Route path={ROUTES.OVERVIEW_ROUTE} element={<OverviewPage />} />
-        <Route path={ROUTES.FLOW_ROUTE} element={<ServiceFlowPage />} />
         <Route path={ROUTES.FOLLOWUPTRAINING_ROUTE} element={<IntentsFollowupTraining />} />
         <Route path="*" element={<NotFoundPage />} />
       </Route >
