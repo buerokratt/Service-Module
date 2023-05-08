8f5dad3ea015e3dda4e0ba6334f1dbe00915b027
baha-a <bhlshrf@gmail.com>
2023-05-08 14:01:19 +0300

    Remove redundant code

--- GUI/src/types/step.ts
+++ GUI/src/types/step.ts
@@ -6,18 +6,3 @@ export interface Step {
   type: StepType;
   action?: string;
 }
-
-export enum StepType {
-  Auth = 'auth',
-  Textfield = 'textfield',
-  Input = 'input',
-  RuleDefinition = 'rule-definition',
-  OpenWebpage = 'open-webpage',
-  FileGenerate = 'file-generate',
-  FileSign = 'file-sign',
-  Step = 'step',
-  Rule = 'rule',
-  FinishingStepEnd = 'finishing-step-end',
-  FinishingStepRedirect = 'finising-step-redirect',
-  RasaRules = 'rasa-rules',
-}
