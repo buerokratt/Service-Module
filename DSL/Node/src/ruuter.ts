7ac8fa27669c61a1e33594911d5e98693f76736c
Mihhail Kohhantsuk <mihhail@devtailor.com>
2023-05-08 15:33:05 +0300

    format

--- DSL/Node/src/ruuter.ts
+++ DSL/Node/src/ruuter.ts
@@ -1,75 +1,76 @@
-import express, { Router } from 'express';
-import path from 'path';
-import { parse as parseYmlToJson } from 'yaml';
-import { getAllFiles, readFile } from './util';
+import express, { Router } from "express";
+import path from "path";
+import { parse as parseYmlToJson } from "yaml";
+import { getAllFiles, readFile } from "./util";
+
 const router: Router = express.Router();
 
-router.get('/list', (req, res) => {
+router.get("/list", (req, res) => {
   const services: any = {};
 
-  getAllFiles('/Ruuter')
-    .filter(filename => filename.endsWith('.yml'))
+  getAllFiles("/Ruuter")
+    .filter((filename) => filename.endsWith(".yml"))
     .map(path.parse)
     .forEach(({ name, dir }) => {
-      const type = dir.includes('/POST/') ? 'POST' : 'GET';
-      const status = dir.endsWith('/inactive') ? 'inactive' : 'active';
+      const type = dir.includes("/POST/") ? "POST" : "GET";
+      const status = dir.endsWith("/inactive") ? "inactive" : "active";
       if (!services[name]) {
-        services[name] = { type, status }
+        services[name] = { type, status };
       } else {
         if (services[name].length > 1) {
-          services[name] = [...services[name], { type, status }]
+          services[name] = [...services[name], { type, status }];
         } else {
-          services[name] = [{ ...services[name] }, { type, status }]
+          services[name] = [{ ...services[name] }, { type, status }];
         }
       }
-    })
+    });
 
-  return res.status(200).json(services)
+  return res.status(200).json(services);
 });
 
-router.get('/sticky', (req, res) => {
+router.get("/sticky", (req, res) => {
   const services: any = {};
 
-  getAllFiles('/Ruuter')
-    .filter(filename => filename.includes('/sticky/'))
-    .filter(filename => filename.endsWith('.yml'))
+  getAllFiles("/Ruuter")
+    .filter((filename) => filename.includes("/sticky/"))
+    .filter((filename) => filename.endsWith(".yml"))
     .map(path.parse)
     .forEach(({ name, dir }) => {
-      const type = dir.includes('/POST/') ? 'POST' : 'GET';
-      const status = dir.endsWith('/inactive') ? 'inactive' : 'active';
+      const type = dir.includes("/POST/") ? "POST" : "GET";
+      const status = dir.endsWith("/inactive") ? "inactive" : "active";
       if (!services[name]) {
-        services[name] = { type, status }
+        services[name] = { type, status };
       } else {
         if (services[name].length > 1) {
-          services[name] = [...services[name], { type, status }]
+          services[name] = [...services[name], { type, status }];
         } else {
-          services[name] = [{ ...services[name] }, { type, status }]
+          services[name] = [{ ...services[name] }, { type, status }];
         }
       }
-    })
+    });
 
-  return res.status(200).json(services)
+  return res.status(200).json(services);
 });
 
-router.post('/sticky/steps', (req, res) => {
-  const name = req.body.name
+router.post("/sticky/steps", (req, res) => {
+  const name = req.body.name;
 
-  const file_path = getAllFiles('/Ruuter')
-    .filter(filename => filename.includes('/sticky/'))
-    .filter(filename => filename.endsWith(`/${name}.yml`))
-    .at(0)
+  const file_path = getAllFiles("/Ruuter")
+    .filter((filename) => filename.includes("/sticky/"))
+    .filter((filename) => filename.endsWith(`/${name}.yml`))
+    .at(0);
 
   if (!file_path) {
-    return res.status(404).json({ message: 'Sticky DSL not found' })
+    return res.status(404).json({ message: "Sticky DSL not found" });
   }
 
   try {
-    const ymlFile = readFile(file_path)
-    const jsonFile = parseYmlToJson(ymlFile)
-    return res.status(200).send(jsonFile)
+    const ymlFile = readFile(file_path);
+    const jsonFile = parseYmlToJson(ymlFile);
+    return res.status(200).send(jsonFile);
   } catch (e) {
-    console.log(e)
-    return res.status(500).send({ message: 'Can\'t read the file' })
+    console.log(e);
+    return res.status(500).send({ message: "Can't read the file" });
   }
 });
 
