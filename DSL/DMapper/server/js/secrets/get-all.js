b36a42b7a50152de0e4ace950bc6bac65c9b17e0
Mihhail Kohhantsuk <mihhail@devtailor.com>
2023-05-09 14:28:35 +0300

    cleanup

--- DSL/DMapper/server/js/secrets/get-all.js
+++ DSL/DMapper/server/js/secrets/get-all.js
@@ -1,11 +1,8 @@
 import { getAllFiles, mapSecretToJson } from "../util/utils.js";
 
 export default async function getAllSecrets() {
-  const prodSecrets = getAllFiles("/secrets/prod");
-  const testSecrets = getAllFiles("/secrets/test");
-
   return {
-    prod: mapSecretToJson(prodSecrets),
-    test: mapSecretToJson(testSecrets),
+    prod: mapSecretToJson(getAllFiles("/secrets/prod")),
+    test: mapSecretToJson(getAllFiles("/secrets/test")),
   };
 }
