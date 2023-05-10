e6d783e3544a0ec23afdadf03c642f4b97f47bd7
Mihhail Kohhantsuk <mihhail@devtailor.com>
2023-05-10 20:07:14 +0300

    return only keys with values

--- DSL/DMapper/server/js/secrets/get-all.js
+++ DSL/DMapper/server/js/secrets/get-all.js
@@ -5,8 +5,9 @@ const flattenSecrets = (data, path, result) => {
     const secretPath = path.length > 0 ? `${path}.${k}` : k;
     if (typeof data[k] === "object") {
       flattenSecrets(data[k], secretPath, result);
+    } else {
+      if (!result.includes(secretPath)) result.push(secretPath);
     }
-    if (!result.includes(secretPath)) result.push(secretPath);
   });
 };
 
