4e2635dd15e5f3d63595645c2b64bc0c93fce9bf
ahmedyasser <26207361+1AhmedYasser@users.noreply.github.com>
2024-03-31 23:15:39 +0200

    Modified Deploy Script

--- DSL/OpenSearch/deploy-opensearch.sh
+++ DSL/OpenSearch/deploy-opensearch.sh
@@ -10,12 +10,14 @@ if [[ -z $URL || -z $AUTH ]]; then
 fi
 
 #services
+curl -XDELETE "$URL/services?ignore_unavailable=true" -u "$AUTH" --insecure
 curl -H "Content-Type: application/x-ndjson" -X PUT "$URL/services" -ku "$AUTH" --data-binary "@fieldMappings/services.json"
 if $MOCK_ALLOWED; then curl -H "Content-Type: application/x-ndjson" -X PUT "$URL/services/_bulk" -ku "$AUTH" --data-binary "@mock/services.json"; fi
-curl -L -X POST '$URL/_scripts/get-services-stat' -H 'Content-Type: application/json' -H 'Cookie: customJwtCookie=test' --data-binary "@templates/get-services-stat.json"
+curl -L -XPOST "$URL/_scripts/get-services-stat" -H 'Content-Type: application/json' -H 'Cookie: customJwtCookie=test' --data-binary "@templates/get-services-stat.json"
 
-#faults
+# #faults
+curl -XDELETE "$URL/faults?ignore_unavailable=true" -u "$AUTH" --insecure
 curl -H "Content-Type: application/x-ndjson" -X PUT "$URL/faults" -ku "$AUTH" --data-binary "@fieldMappings/faults.json"
 if $MOCK_ALLOWED; then curl -H "Content-Type: application/x-ndjson" -X PUT "$URL/faults/_bulk" -ku "$AUTH" --data-binary "@mock/faults.json"; fi
-curl -L -X POST '$URL/_scripts/get-faults-by-request-id' -H 'Content-Type: application/json' -H 'Cookie: customJwtCookie=test' --data-binary "@templates/get-faults-by-request-id.json"
-curl -L -X POST '$URL/_scripts/get-faults-by-level' -H 'Content-Type: application/json' -H 'Cookie: customJwtCookie=test' --data-binary "@templates/get-faults-by-level.json"
+curl -L -XPOST "$URL/_scripts/get-faults-by-request-id" -H 'Content-Type: application/json' -H 'Cookie: customJwtCookie=test' --data-binary "@templates/get-faults-by-request-id.json"
+curl -L -XPOST "$URL/_scripts/get-faults-by-level" -H 'Content-Type: application/json' -H 'Cookie: customJwtCookie=test' --data-binary "@templates/get-faults-by-level.json"
