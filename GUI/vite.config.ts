5d2e0fcf1d125d30c11d086a5aaa3ffffdbd5143
ahmedyasser <26207361+1AhmedYasser@users.noreply.github.com>
2024-05-08 10:43:29 +0300

    Enhanced CSP

--- GUI/vite.config.ts
+++ GUI/vite.config.ts
@@ -23,8 +23,7 @@ export default ({ mode }) => {
       strictPort: true,
       port: parseInt(process.env.REACT_APP_APP_PORT),
       headers: {
-        "Content-Security-Policy":
-          "upgrade-insecure-requests; default-src 'self'; font-src 'self' data:; img-src 'self' data:; script-src 'self' 'unsafe-eval' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; object-src 'none'; connect-src 'self' http://localhost:8086 http://localhost:8085 https://admin.dev.buerokratt.ee/chat/menu.json;",
+        "Content-Security-Policy": process.env.REACT_APP_CSP,
       },
     },
     build: {
