c4a9d03cd5a4db9d52f3fe782496be22de8d05b2
baha-a <bhlshrf@gmail.com>
2024-05-15 08:55:43 +0300

    Replace UPDATE with INSERT for services table

--- DSL/Resql/services/set-status.sql
+++ DSL/Resql/services/set-status.sql
@@ -1,3 +1,14 @@
-UPDATE services
-SET current_state = :new_state::service_state
+INSERT INTO services (name, description, ruuter_type, current_state, service_id, is_common, structure, endpoints)
+SELECT
+  name,
+  description,
+  ruuter_type,
+  :new_state::service_state,
+  service_id,
+  is_common,
+  structure,
+  endpoints
+FROM services
 WHERE service_id = :id
+ORDER BY id DESC
+LIMIT 1;
--- DSL/Resql/services/update-service-endpoints.sql
+++ DSL/Resql/services/update-service-endpoints.sql
@@ -1,3 +1,14 @@
-UPDATE services
-SET endpoints = :endpoints::json
-WHERE service_id = :id;
+INSERT INTO services (name, description, ruuter_type, current_state, service_id, is_common, structure, endpoints)
+SELECT
+  name,
+  description,
+  ruuter_type,
+  current_state,
+  service_id,
+  is_common,
+  structure,
+  :endpoints::json
+FROM services
+WHERE service_id = :id
+ORDER BY id DESC
+LIMIT 1;
