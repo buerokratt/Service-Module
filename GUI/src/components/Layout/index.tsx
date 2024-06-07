2f991b51ad2ee9a57f283f28426ea35ed17f6fce
baha-a <bhlshrf@gmail.com>
2024-06-07 13:49:13 +0200

    Update menu package version and hide some menu items from production

--- GUI/src/components/Layout/index.tsx
+++ GUI/src/components/Layout/index.tsx
@@ -1,64 +1,35 @@
-import { FC, useState, ReactNode, PropsWithChildren } from 'react';
+import React, { FC, ReactNode, PropsWithChildren } from 'react';
+import useStore from '../../store/store';
 import { Outlet } from 'react-router-dom';
 import { MainNavigation } from '@buerokratt-ria/menu';
 import { Header } from '@buerokratt-ria/header'
-import {useQuery} from "@tanstack/react-query";
-import useStore from '../../store/store';
 import useToastStore from 'store/toasts.store';
 import './Layout.scss';
 
-  type LayoutProps = {
-    disableMenu?: boolean;
-    customHeader?: ReactNode;
-  };
+type LayoutProps = {
+  disableMenu?: boolean;
+  customHeader?: ReactNode;
+};
 
-
-  const Layout: FC<PropsWithChildren<LayoutProps>> = ({
-                                                        disableMenu,
-                                                        customHeader,
-                                                        children,
-                                                      }) => {
-
-    const CACHE_NAME = 'mainmenu-cache';
-
-    const [mainMenuItems, setMainMenuItems] = useState([])
-
-    useQuery({
-      queryKey: [import.meta.env.REACT_APP_MENU_URL + import.meta.env.REACT_APP_MENU_PATH],
-      onSuccess: (res: any) => {
-        try {
-          setMainMenuItems(res);
-          localStorage.setItem(CACHE_NAME, JSON.stringify(res));
-        } catch (e) {
-          console.log(e);
-        }
-      },
-      onError: (error: any) => {
-        setMainMenuItems(getCache());
+const Layout: FC<PropsWithChildren<LayoutProps>> = ({
+    disableMenu,
+    customHeader,
+    children,
+  }) => (
+  <div className="layout">
+    {!disableMenu && <MainNavigation />}
+    <div className="layout__wrapper">
+      {
+        customHeader ?? <Header
+            toastContext={{ open: useToastStore.getState().open }}
+            user={useStore.getState().userInfo}
+          />
       }
-
-    });
-
-    function getCache(): any {
-      const cache = localStorage.getItem(CACHE_NAME) ?? '{}';
-      return JSON.parse(cache);
-    }
-
-    return (
-        <div className="layout">
-          {!disableMenu &&
-              <MainNavigation serviceId={import.meta.env.REACT_APP_SERVICE_ID.split(',')} items={mainMenuItems}/>}
-          <div className="layout__wrapper">
-            {customHeader ?? <Header
-                toastContext={{ open: useToastStore.getState().open }}
-                user={useStore.getState().userInfo}
-            />}
-            <main className="layout__main">
-              {children ?? <Outlet/>}
-            </main>
-          </div>
-        </div>
-    )
-  };
+      <main className="layout__main">
+        {children ?? <Outlet/>}
+      </main>
+    </div>
+  </div>
+);
 
 export default Layout;
