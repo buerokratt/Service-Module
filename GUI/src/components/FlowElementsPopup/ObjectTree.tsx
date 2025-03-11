e3854b7d6505a9a525e51427a6fdc898d0db93a3
Igor Krupenja <igor.krupenja@gmail.com>
2025-03-11 15:25:34 +0200

    PoC string

--- GUI/src/components/FlowElementsPopup/ObjectTree.tsx
+++ GUI/src/components/FlowElementsPopup/ObjectTree.tsx
@@ -3,7 +3,6 @@ import { CSSProperties, FC, useState } from "react";
 import { JSONTree, KeyPath } from "react-json-tree";
 import "./styles.scss";
 import { stringToTemplate } from "utils/string-util";
-import { FormCheckbox } from "components/FormElements";
 
 const theme = {
   base00: "black",
@@ -28,19 +27,54 @@ const getKeyPathString = (keyPath: KeyPath) => {
   return keyPath.toReversed().join(".");
 };
 
+const round = (n: number) => {
+  return Math.round((n + Number.EPSILON) * 100) / 100;
+};
+
 type ObjectTreeProps = {
   data: object;
   path: string | number;
   style?: CSSProperties;
 };
 
+// todo remove
+// const fakeData = {
+//   a: 1.123456789,
+//   b: 2.123456789,
+//   c: 3.123456789,
+//   d: 4.123456789,
+//   e: 5.123456789,
+//   f: {
+//     a: 1.123456789,
+//     b: 2.123456789,
+//     c: 3.123456789,
+//     d: 4.123456789,
+//     e: 5.123456789,
+//   },
+//   bool: true,
+//   null: null,
+//   undefined: undefined,
+//   array: [1, 2, 3, 4, 5],
+//   object: {
+//     a: 1,
+//   },
+// };
+
 export const ObjectTree: FC<ObjectTreeProps> = ({ path, data, style }) => {
   const pathArray = String(path).split(".");
   const root = pathArray.pop()!;
-  const [roundedValues, setRoundedValues] = useState<Record<string, number>>({});
+  const [roundedValues, setRoundedValues] = useState<Map<string, number>>(new Map());
   // todo maybe test escaping
 
-  console.log("igor round", roundedValues);
+  console.log("igor roundedValues", Object.fromEntries(roundedValues));
+
+  const buildKeyPathString = (keyPath: KeyPath) => {
+    const base = pathArray.join(".") + "." + getKeyPathString(keyPath);
+    const isRounded = roundedValues.has(getKeyPathString(keyPath));
+    if (!isRounded) return stringToTemplate(base);
+
+    return stringToTemplate("Math.round((" + base + " + Number.EPSILON) * 100) / 100)");
+  };
 
   return (
     <div style={{ padding: "0px 15px 5px", ...style }}>
@@ -52,14 +86,13 @@ export const ObjectTree: FC<ObjectTreeProps> = ({ path, data, style }) => {
         labelRenderer={(keyPath) => (
           <OutputElementBox
             text={String(keyPath[0]) + ":"}
-            value={stringToTemplate(pathArray.join(".") + "." + getKeyPathString(keyPath))}
+            value={buildKeyPathString(keyPath)}
             useValue
             draggable={true}
             className="object-tree-chip"
           />
         )}
         valueRenderer={(raw, _, ...keyPath) => {
-          // console.log("igor render", _, keyPath);
           return typeof raw === "number" && !Number.isInteger(raw) ? (
             <>
               {/* todo style */}
@@ -72,17 +105,22 @@ export const ObjectTree: FC<ObjectTreeProps> = ({ path, data, style }) => {
               <input
                 type="checkbox"
                 onClick={(e) => {
+                  // todo extract
                   const target = e.target as HTMLInputElement;
                   const keyPathString = getKeyPathString(keyPath);
-                  setRoundedValues((prev) =>
-                    target.checked
-                      ? { ...prev, [keyPathString]: Math.round((raw + Number.EPSILON) * 100) / 100 }
-                      : Object.fromEntries(Object.entries(prev).filter(([k]) => k !== keyPathString))
-                  );
+                  setRoundedValues((prev) => {
+                    const newMap = new Map(prev);
+                    if (target.checked) {
+                      newMap.set(keyPathString, round(raw));
+                    } else {
+                      newMap.delete(keyPathString);
+                    }
+                    return newMap;
+                  });
                 }}
               />
               <span style={{ color: "black", fontStyle: "italic" }}>Round </span>
-              <span>{String(raw)}</span>
+              <span>{roundedValues.has(getKeyPathString(keyPath)) ? round(raw) : raw}</span>
             </>
           ) : (
             <span>{String(raw)}</span>
