754db47ede5041fca9c8659727d56be7f695314f
Igor Krupenja <igor.krupenja@gmail.com>
2025-03-19 16:53:23 +0200

    todo

--- GUI/src/services/service-builder.ts
+++ GUI/src/services/service-builder.ts
@@ -1037,6 +1037,9 @@ export const saveFlowClick = async () => {
   const edges = useServiceStore.getState().edges;
   const nodes = useServiceStore.getState().nodes;
 
+  // todo test: saving non-common endpoints
+  // todo bug1: common endpoints path is wrong, trying to point to non-existing non-common
+
   await saveFlow({
     steps,
     name,
