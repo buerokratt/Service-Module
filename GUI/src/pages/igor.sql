1c5d98566b6ec69267ff1fc243cfef1697d8debf
Igor Krupenja <igor.krupenja@gmail.com>
2025-04-09 14:46:08 +0300

    tmp

--- GUI/src/pages/igor.sql
+++ GUI/src/pages/igor.sql
@@ -1,6 +1,7 @@
 SELECT *
 FROM services
 WHERE jsonb_path_exists(
-    structure::jsonb,
-    '$.**.originalDefinedNodeId ? (@ == "cb1534cd-b5b2-43e0-96c4-32cc21dd3f66")'
-);
+        structure::jsonb,
+        '$.**.originalDefinedNodeId ? (@ == "cb1534cd-b5b2-43e0-96c4-32cc21dd3f66")'
+      )
+  AND service_id != 'bf226d47-ee80-48be-9861-1e08b8fcaa87';
