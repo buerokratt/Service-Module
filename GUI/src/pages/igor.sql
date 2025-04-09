8d05f45c9fac240cbcd733a1d77bfe528a078486
Igor Krupenja <igor.krupenja@gmail.com>
2025-04-09 16:17:40 +0300

    gs

--- GUI/src/pages/igor.sql
+++ GUI/src/pages/igor.sql
@@ -1,7 +1,8 @@
-SELECT *
+SELECT DISTINCT ON (service_id) *
 FROM services
 WHERE jsonb_path_exists(
         structure::jsonb,
         '$.**.originalDefinedNodeId ? (@ == "cb1534cd-b5b2-43e0-96c4-32cc21dd3f66")'
       )
-  AND service_id != 'bf226d47-ee80-48be-9861-1e08b8fcaa87';
+  AND service_id != 'bf226d47-ee80-48be-9861-1e08b8fcaa87'
+ORDER BY service_id, id DESC;
