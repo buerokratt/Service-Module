27d279f9c60f1fb1beb1f3a27f9fc918033133f6
Igor Krupenja <igor.krupenja@gmail.com>
2025-04-10 13:27:46 +0300

    PoC SQL

--- GUI/src/pages/igor.sql
+++ GUI/src/pages/igor.sql
@@ -3,9 +3,9 @@ SELECT DISTINCT ON (service_id) name, service_id
 FROM services
 WHERE jsonb_path_exists(
         structure::jsonb,
-        '$.**.originalDefinedNodeId ? (@ == "' || :node_id || '")'
+        ('$.**.originalDefinedNodeId ? (@ == "' || :node_id || '")')::jsonpath
       )
-  AND service_id != :excluded_service_id
+  AND service_id != :service_id
 ORDER BY service_id, id DESC;
 
 -- for deletion
