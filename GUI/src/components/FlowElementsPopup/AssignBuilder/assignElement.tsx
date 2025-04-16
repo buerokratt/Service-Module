655863381cb5060ac2b37438a7fc310a6d2ebdfa
Igor Krupenja <igor.krupenja@gmail.com>
2025-04-16 16:10:31 +0300

    wip draggable

--- GUI/src/components/FlowElementsPopup/AssignBuilder/assignElement.tsx
+++ GUI/src/components/FlowElementsPopup/AssignBuilder/assignElement.tsx
@@ -1,7 +1,9 @@
-import React from 'react';
-import { FormInput, Icon, Track } from 'components';
-import { MdDeleteOutline } from 'react-icons/md';
+import React from "react";
+import { FormInput, Icon, OutputElementBox, Track } from "components";
+import { MdDeleteOutline } from "react-icons/md";
 import { Assign } from "./assign-types";
+import "../styles.scss";
+import DraggableInput from "components/FormElements/DraggableInput";
 
 interface AssignElementProps {
   element: Assign;
@@ -10,27 +12,37 @@ interface AssignElementProps {
 }
 
 const AssignElement: React.FC<AssignElementProps> = ({ element, onRemove, onChange }) => {
-  
   const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
     change(e.target.name, e.target.value);
-  }
+  };
 
   const change = (name: string, value?: string) => {
     onChange({ ...element, [name]: value });
-  }
+  };
 
   return (
     <Track gap={16} isFlex>
       <Track gap={16} isFlex>
         <FormInput value={element.key} name="key" onChange={handleChange} label="" hideLabel />
-        :
-        <FormInput value={element.value} name="value" onChange={handleChange} label="" hideLabel />
+        {element.value ? (
+          <OutputElementBox text={element.value} color="green" draggable={true} />
+        ) : (
+          <DraggableInput
+            value={element.value}
+            name="value"
+            onChange={handleChange}
+            label=""
+            placeholder="Drag element here"
+            hideLabel
+            className="assign-value-input"
+          />
+        )}
       </Track>
       <button onClick={() => onRemove(element.id)} className="small-assign-button assign-red">
         <Icon icon={<MdDeleteOutline />} />
       </button>
     </Track>
   );
-}
+};
 
 export default AssignElement;
---
+++ GUI/src/components/FormElements/DraggableInput/index.tsx
@@ -0,0 +1,56 @@
+import React, { forwardRef, InputHTMLAttributes } from "react";
+import clsx from "clsx";
+import "./FormInput.scss";
+
+type DraggableInputProps = InputHTMLAttributes<HTMLInputElement> & {
+  label: string;
+  name: string;
+  hideLabel?: boolean;
+};
+
+const DraggableInput = forwardRef<HTMLInputElement, DraggableInputProps>(({ label, name, hideLabel, ...rest }, ref) => {
+  const inputClasses = clsx("input");
+
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    // Prevent any keyboard input
+    e.preventDefault();
+  };
+
+  // const handleDrop = (e: React.DragEvent<HTMLInputElement>) => {
+  //   e.preventDefault();
+  //   const value = e.dataTransfer.getData("text/plain");
+  //   if (value) {
+  //     // Trigger onChange with the dropped value
+  //     const event = {
+  //       target: {
+  //         name,
+  //         value,
+  //       },
+  //     } as React.ChangeEvent<HTMLInputElement>;
+  //     rest.onChange?.(event);
+  //   }
+  // };
+
+  return (
+    <div className={inputClasses}>
+      {label && !hideLabel && <label className="input__label">{label}</label>}
+      <div className="input__wrapper">
+        <input
+          className={inputClasses}
+          name={name}
+          ref={ref}
+          aria-label={hideLabel ? label : undefined}
+          onKeyDown={handleKeyDown}
+          // onDrop={handleDrop}
+          onDragOver={(e) => e.preventDefault()}
+          readOnly
+          {...rest}
+        />
+      </div>
+    </div>
+  );
+});
+
+DraggableInput.displayName = "DraggableInput";
+
+export default DraggableInput;
