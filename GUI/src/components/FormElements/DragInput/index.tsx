0b8dc8fb04e57af386018c428afc004f8c96f00e
Igor Krupenja <igor.krupenja@gmail.com>
2025-04-16 16:23:46 +0300

    cl

--- GUI/src/components/FormElements/DragInput/index.tsx
+++ GUI/src/components/FormElements/DragInput/index.tsx
@@ -1,6 +1,6 @@
 import React, { forwardRef, InputHTMLAttributes } from "react";
 import clsx from "clsx";
-import "./FormInput.scss";
+import { FormInput } from "components";
 
 type DraggableInputProps = InputHTMLAttributes<HTMLInputElement> & {
   label: string;
@@ -8,52 +8,17 @@ type DraggableInputProps = InputHTMLAttributes<HTMLInputElement> & {
   hideLabel?: boolean;
 };
 
-const DragInput = forwardRef<HTMLInputElement, DraggableInputProps>(({ label, name, hideLabel, ...rest }, ref) => {
-  const inputClasses = clsx("input");
-
-  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
-    // Prevent any keyboard input
-    e.preventDefault();
-  };
-
-  const handleFocus = (e: React.FocusEvent<HTMLInputElement>) => {
-    // Prevent focus
-    e.target.blur();
-  };
-
-  // const handleDrop = (e: React.DragEvent<HTMLInputElement>) => {
-  //   e.preventDefault();
-  //   const value = e.dataTransfer.getData("text/plain");
-  //   if (value) {
-  //     // Trigger onChange with the dropped value
-  //     const event = {
-  //       target: {
-  //         name,
-  //         value,
-  //       },
-  //     } as React.ChangeEvent<HTMLInputElement>;
-  //     rest.onChange?.(event);
-  //   }
-  // };
-
+// todo props type
+const DragInput = forwardRef<HTMLInputElement, DraggableInputProps>(({ ...rest }, ref) => {
   return (
-    <div className={inputClasses}>
-      {label && !hideLabel && <label className="input__label">{label}</label>}
-      <div className="input__wrapper">
-        <input
-          className={inputClasses}
-          name={name}
-          ref={ref}
-          aria-label={hideLabel ? label : undefined}
-          onKeyDown={handleKeyDown}
-          onFocus={handleFocus}
-          onDragOver={(e) => e.preventDefault()}
-          tabIndex={-1}
-          // onDrop={handleDrop}
-          {...rest}
-        />
-      </div>
-    </div>
+    <FormInput
+      // Disables focus, text cursor and everything related to keyboard input
+      tabIndex={-1}
+      onFocus={(e) => e.target.blur()}
+      onDragOver={(e) => e.preventDefault()}
+      {...rest}
+      ref={ref}
+    />
   );
 });
 
