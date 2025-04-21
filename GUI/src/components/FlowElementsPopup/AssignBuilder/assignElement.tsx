8e53af87aa7a6075810b229f7fac035fca1c6d9b
Igor Krupenja <igor.krupenja@gmail.com>
2025-04-21 17:54:10 +0300

    man

--- GUI/src/components/FlowElementsPopup/AssignBuilder/assignElement.tsx
+++ GUI/src/components/FlowElementsPopup/AssignBuilder/assignElement.tsx
@@ -4,7 +4,7 @@ import { MdDeleteOutline, MdEdit, MdMoveDown } from "react-icons/md";
 import { Assign } from "../../../types/assign";
 import "../styles.scss";
 import { stringToTemplate, templateToString } from "utils/string-util";
-import { isArray, isObject, normalizeObjectPath } from "utils/object-util";
+import { isArray, isObject } from "utils/object-util";
 
 interface AssignElementProps {
   element: Assign;
@@ -27,12 +27,11 @@ const AssignElement: React.FC<AssignElementProps> = ({ element, onRemove, onChan
 
   // todo - handle VALUE
   const changeSecondSlot = (data: Assign) => {
-    // // const value = stringToTemplate(templateToString(element.value) + '["' + templateToString(data.value) + '"]');
-    // const value = stringToTemplate(templateToString(element.value) + normalizeObjectPath(templateToString(data.value)));
-    // console.log("old value", element.value);
-    // console.log("new value", data.value);
-    // console.log("PROCESSED value", value);
-    // onChange({ ...element, value, slots: [slots[0]!, data] });
+    const value = stringToTemplate(templateToString(element.value) + '["' + templateToString(data.value) + '"]');
+    console.log("old value", element.value);
+    console.log("new value", data.value);
+    console.log("PROCESSED value", value);
+    onChange({ ...element, value, slots: [slots[0]!, data] });
   };
 
   const resetSecondSlot = () => {
@@ -44,27 +43,33 @@ const AssignElement: React.FC<AssignElementProps> = ({ element, onRemove, onChan
     <Track gap={16} isFlex>
       <FormInput value={element.key} name="key" onChange={changeKey} label="" hideLabel />:
       <Track style={{ flex: "1 0 75%", justifyContent: "flex-end" }}>
-        <Track gap={3} isFlex>
-          <DragInput disallowedId={element.id} element={slots[0]} onChange={(value) => changeFirstSlot(value)} />
-          {slots.length && isObject(slots[0]?.data) && !isArray(slots[0]?.data) ? (
-            <button
-              onClick={() => {
-                setIsSecondSlotOpen(!isSecondSlotOpen);
-                if (!isSecondSlotOpen) resetSecondSlot();
-              }}
-              className={`small-assign-button ${isSecondSlotOpen ? "assign-red" : "assign-blue"}`}
-            >
-              <Icon icon={<MdMoveDown />} />
-            </button>
-          ) : null}
-          {isSecondSlotOpen ? (
-            <DragInput disallowedId={element.id} element={slots[1]} onChange={(value) => changeSecondSlot(value)} />
-          ) : null}
-        </Track>
+        {isEditingManually ? (
+          <div>tada!</div>
+        ) : (
+          <Track gap={3} isFlex>
+            <DragInput disallowedId={element.id} element={slots[0]} onChange={(value) => changeFirstSlot(value)} />
+            {slots.length && isObject(slots[0]?.data) && !isArray(slots[0]?.data) ? (
+              <button
+                onClick={() => {
+                  setIsSecondSlotOpen(!isSecondSlotOpen);
+                  if (!isSecondSlotOpen) resetSecondSlot();
+                }}
+                className={`small-assign-button ${isSecondSlotOpen ? "assign-red" : "assign-blue"}`}
+              >
+                <Icon icon={<MdMoveDown />} />
+              </button>
+            ) : null}
+            {isSecondSlotOpen ? (
+              <DragInput disallowedId={element.id} element={slots[1]} onChange={(value) => changeSecondSlot(value)} />
+            ) : null}
+          </Track>
+        )}
         {/* todo manual edit */}
-        <button onClick={() => null} className="small-assign-button assign-blue">
-          <Icon icon={<MdEdit />} />
-        </button>
+        {!isEditingManually ? (
+          <button onClick={() => setIsEditingManually(true)} className="small-assign-button assign-blue">
+            <Icon icon={<MdEdit />} />
+          </button>
+        ) : null}
         <button onClick={() => onRemove(element.id)} className="small-assign-button assign-red">
           <Icon icon={<MdDeleteOutline />} />
         </button>
