a036eabbecc9eba5b720452747944e63ad4aab51
Igor Krupenja <igor.krupenja@gmail.com>
2025-04-30 15:07:43 +0300

    fix

--- DSL/Resql/services/POST/get-services-by-endpoint-id.sql
+++ DSL/Resql/services/POST/get-services-by-endpoint-id.sql
@@ -1,13 +1,13 @@
-SELECT DISTINCT ON (service_id) name, service_id
-FROM services s1
+WITH latest_services AS (
+    SELECT DISTINCT ON (service_id) id, name, service_id, structure
+    FROM services
+    WHERE deleted IS false
+    ORDER BY service_id, id DESC
+)
+SELECT name, service_id
+FROM latest_services
 WHERE jsonb_path_exists(
         structure::jsonb,
         ('$.**.originalDefinedNodeId ? (@ == "' || :endpoint_id || '")')::jsonpath
       )
-  AND (:excluded_service_id::text IS NULL OR service_id != :excluded_service_id)
-  AND NOT EXISTS (
-    SELECT 1 FROM services s2 
-    WHERE s2.service_id = s1.service_id 
-    AND s2.deleted IS true
-  )
-ORDER BY service_id, id DESC;
+  AND (:excluded_service_id::text IS NULL OR service_id != :excluded_service_id);
