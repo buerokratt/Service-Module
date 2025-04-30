3b993dc3ccb48e7402050f9c124e79063f1eb617
Igor Krupenja <igor.krupenja@gmail.com>
2025-04-30 14:31:59 +0300

    fix

--- DSL/Resql/services/POST/get-services-by-endpoint-id.sql
+++ DSL/Resql/services/POST/get-services-by-endpoint-id.sql
@@ -5,4 +5,5 @@ WHERE jsonb_path_exists(
         ('$.**.originalDefinedNodeId ? (@ == "' || :endpoint_id || '")')::jsonpath
       )
   AND (:excluded_service_id::text IS NULL OR service_id != :excluded_service_id)
+  AND deleted IS false
 ORDER BY service_id, id DESC;
