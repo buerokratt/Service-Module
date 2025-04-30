217b72faf9a6b9dbabc972ed29ada2c4e224a600
Igor Krupenja <igor.krupenja@gmail.com>
2025-04-30 14:46:20 +0300

    bete

--- DSL/Resql/services/POST/get-services-by-endpoint-id.sql
+++ DSL/Resql/services/POST/get-services-by-endpoint-id.sql
@@ -1,9 +1,13 @@
 SELECT DISTINCT ON (service_id) name, service_id
-FROM services
+FROM services s1
 WHERE jsonb_path_exists(
         structure::jsonb,
         ('$.**.originalDefinedNodeId ? (@ == "' || :endpoint_id || '")')::jsonpath
       )
   AND (:excluded_service_id::text IS NULL OR service_id != :excluded_service_id)
-  AND deleted IS false
+  AND NOT EXISTS (
+    SELECT 1 FROM services s2 
+    WHERE s2.service_id = s1.service_id 
+    AND s2.deleted IS false
+  )
 ORDER BY service_id, id DESC;
