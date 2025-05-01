9c3bf6e31bbe967c0a7c1c5e62d44bc09cb68c18
Igor Krupenja <igor.krupenja@gmail.com>
2025-05-01 16:05:58 +0300

    fix drag in input

--- GUI/src/components/OutputElementBox/index.tsx
+++ GUI/src/components/OutputElementBox/index.tsx
@@ -2,7 +2,7 @@ import { CSSProperties, FC, DragEvent, ReactNode } from "react";
 import Box from "../Box";
 import { Assign, StepType } from "types";
 import useServiceStore from "store/new-services.store";
-import { ASSIGN_DRAG_TYPE } from "utils/component-util";
+import { ASSIGN_DRAG_TYPE, encodeDragData } from "utils/component-util";
 
 type OutputElementBoxProps = {
   readonly children: ReactNode;
@@ -36,10 +36,16 @@ const OutputElementBox: FC<OutputElementBoxProps> = ({
   const handleDragStart = (event: DragEvent<HTMLDivElement>) => {
     if (!dragData) return;
 
+    // todo check usages
+    // todo check message
     event.dataTransfer.setData(
-      ASSIGN_DRAG_TYPE,
-      node?.data.stepType === StepType.Assign ? JSON.stringify(dragData) : dragData.value
+      encodeDragData(node?.data.stepType === StepType.Assign ? JSON.stringify(dragData) : dragData.value),
+      ""
     );
+    // event.dataTransfer.setData(
+    //   ASSIGN_DRAG_TYPE,
+    //   node?.data.stepType === StepType.Assign ? JSON.stringify(dragData) : dragData.value
+    // );
   };
 
   return (
