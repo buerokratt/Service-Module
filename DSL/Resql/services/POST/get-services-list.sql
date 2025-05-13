9acc2a5a7cf858cac9e550fd9aa2a18a272487df
Igor Krupenja <igor.krupenja@gmail.com>
2025-05-13 14:07:48 +0300

    poc

--- DSL/Resql/services/POST/get-services-list.sql
+++ DSL/Resql/services/POST/get-services-list.sql
@@ -4,34 +4,13 @@ WITH MaxServices AS (
   GROUP BY service_id
 )
 SELECT
-  id,
   name,
   description,
-  slot,
   current_state AS state,
-  ruuter_type AS type,
-  is_common AS isCommon,
-  structure::json,
-  subquery.endpoints::json AS endpoints,
   service_id,
   CEIL(COUNT(*) OVER() / :page_size::DECIMAL) AS total_pages
 FROM services
 JOIN MaxServices ON id = maxId
-JOIN (
-  SELECT jsonb_agg(endpoint) AS endpoints
-  FROM (
-    SELECT DISTINCT endpoint
-    FROM (
-      SELECT endpoint::jsonb
-      FROM services, json_array_elements(endpoints) AS endpoint
-      WHERE (endpoint->>'isCommon')::boolean = true
-      UNION
-      SELECT endpoint::jsonb
-      FROM services, json_array_elements(endpoints) AS endpoint, MaxServices
-      WHERE id = maxId
-    ) AS combined_endpoints
-  ) subquery
-) subquery ON true
 WHERE NOT deleted AND NOT is_common
 ORDER BY 
    CASE WHEN :sorting = 'id asc' THEN id END ASC,
