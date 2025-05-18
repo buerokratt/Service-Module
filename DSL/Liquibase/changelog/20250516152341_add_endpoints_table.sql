c4e4c32fdf2670263bb414ad838a63698e87e0c4
Igor Krupenja <igor.krupenja@gmail.com>
2025-05-18 16:01:04 +0300

    wip table

--- DSL/Liquibase/changelog/20250516152341_add_endpoints_table.sql
+++ DSL/Liquibase/changelog/20250516152341_add_endpoints_table.sql
@@ -3,14 +3,18 @@
 CREATE TABLE endpoints (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     service_ids UUID [] NOT NULL,
-    -- todo varchar or text or wut?
-    name VARCHAR(255) NOT NULL,
+    name TEXT NOT NULL,
     -- todo enum?
-    type VARCHAR(255) NOT NULL,
-    file_name VARCHAR(255) NOT NULL,
+    type TEXT NOT NULL,
+    file_name TEXT NOT NULL,
     is_common BOOLEAN NOT NULL DEFAULT FALSE,
     definitions JSONB NOT NULL DEFAULT '[]',
     deleted BOOLEAN NOT NULL DEFAULT FALSE,
     created_at TIMESTAMP DEFAULT current_timestamp,
     updated_at TIMESTAMP DEFAULT current_timestamp
 );
+
+CREATE INDEX idx_endpoints_is_common ON endpoints (is_common);
+CREATE INDEX idx_endpoints_service_ids ON endpoints USING gin (service_ids);
+CREATE INDEX idx_endpoints_created_at ON endpoints (created_at);
+CREATE INDEX idx_endpoints_deleted ON endpoints (deleted);
