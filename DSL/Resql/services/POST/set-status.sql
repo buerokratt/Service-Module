bcda61d82002776423d8706629ae7755e712f0fb
Igor Krupenja <igor.krupenja@gmail.com>
2025-05-21 11:33:20 +0300

    wip sql

--- DSL/Resql/services/POST/set-status.sql
+++ DSL/Resql/services/POST/set-status.sql
@@ -1,15 +1,23 @@
-INSERT INTO services (name, description, slot, ruuter_type, current_state, service_id, is_common, structure, endpoints)
+INSERT INTO services (
+    name,
+    description,
+    slot,
+    ruuter_type,
+    current_state,
+    service_id,
+    is_common,
+    structure
+)
 SELECT
-  name,
-  description,
-  slot,
-  ruuter_type,
-  :new_state::service_state,
-  service_id,
-  is_common,
-  structure,
-  endpoints
+    name,
+    description,
+    slot,
+    ruuter_type,
+    : new_state::service_state,
+    service_id,
+    is_common,
+    structure
 FROM services
-WHERE service_id = :id
+WHERE service_id =: id
 ORDER BY id DESC
 LIMIT 1;
