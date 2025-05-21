0c4b488f8ecbbceb180e84ce324d4db45c9f97fc
Igor Krupenja <igor.krupenja@gmail.com>
2025-05-21 12:13:13 +0300

    wip sql

--- DSL/Resql/services/POST/get-service-by-id.sql
+++ DSL/Resql/services/POST/get-service-by-id.sql
@@ -13,24 +13,8 @@ SELECT
   ruuter_type AS type,
   is_common AS isCommon,
   structure::json,
-  subquery.endpoints::json AS endpoints,
   service_id
 FROM services
 JOIN MaxService ON id = maxId
-JOIN (
-  SELECT jsonb_agg(endpoint) AS endpoints
-  FROM (
-    SELECT DISTINCT endpoint
-    FROM (
-      SELECT endpoint::jsonb
-      FROM services, json_array_elements(endpoints) AS endpoint
-      WHERE (endpoint->>'isCommon')::boolean = true
-      UNION
-      SELECT endpoint::jsonb
-      FROM services, json_array_elements(endpoints) AS endpoint, MaxService
-      WHERE id = maxId
-    ) AS combined_endpoints
-  ) subquery
-) subquery ON true
-WHERE NOT deleted 
+WHERE NOT deleted
 ORDER BY id ASC;
