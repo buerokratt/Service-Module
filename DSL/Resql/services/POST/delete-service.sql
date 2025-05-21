feb43fda18966d48c3c9440c867c4f957000d5cf
Igor Krupenja <igor.krupenja@gmail.com>
2025-05-21 11:21:33 +0300

    wip sql

--- DSL/Resql/services/POST/delete-service.sql
+++ DSL/Resql/services/POST/delete-service.sql
@@ -1,18 +1,29 @@
-INSERT INTO services (name, description, slot, created_at, updated_at, ruuter_type, current_state, service_id, is_common, deleted, structure, endpoints)
-SELECT 
-  name,
-  description,
-  slot,
-  created_at,
-  updated_at,
-  ruuter_type,
-  current_state,
-  service_id,
-  is_common,
-  TRUE AS deleted,
-  structure,
-  endpoints
+INSERT INTO services (
+    name,
+    description,
+    slot,
+    created_at,
+    updated_at,
+    ruuter_type,
+    current_state,
+    service_id,
+    is_common,
+    deleted,
+    structure
+)
+SELECT
+    name,
+    description,
+    slot,
+    created_at,
+    updated_at,
+    ruuter_type,
+    current_state,
+    service_id,
+    is_common,
+    TRUE AS deleted,
+    structure
 FROM services
-WHERE service_id = :id
+WHERE service_id =: id
 ORDER BY id DESC
 LIMIT 1;
