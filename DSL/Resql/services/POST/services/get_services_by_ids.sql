baeef5e6aa71cc7b8eec3f6934c9df5efc71f774
Igor Krupenja <igor.krupenja@gmail.com>
2025-05-27 17:48:17 +0300

    fix second query

--- DSL/Resql/services/POST/services/get_services_by_ids.sql
+++ DSL/Resql/services/POST/services/get_services_by_ids.sql
@@ -1,7 +1,7 @@
 WITH latest_services AS (
   SELECT DISTINCT ON (service_id) id, name, service_id
   FROM services
-  WHERE service_id = ANY(:serviceIds)
+  WHERE service_id = ANY(ARRAY['698a8ce0-c7f2-4663-b07b-b22ce2eee1de', 'another-id'])
     AND deleted IS FALSE
   ORDER BY service_id, id DESC
 )
