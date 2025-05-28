68f83e6359e56b64336a780d0da0d2e254aaf749
Artsiom Beida <artsiom.beida@mindtitan.com>
2025-05-28 13:40:09 +0200

    implemented security-001 for users resql

--- DSL/Resql/users/GET/config/get_configuration.sql
+++ DSL/Resql/users/GET/config/get_configuration.sql
@@ -1,3 +1,30 @@
+/*
+declaration:
+  version: 0.1
+  description: "Fetch the latest non-deleted configuration value for a specified key"
+  method: get
+  namespace: config
+  returns: json
+  allowlist:
+    query:
+      - field: key
+        type: string
+        description: "Configuration key to retrieve the latest value for"
+  response:
+    fields:
+      - field: id
+        type: string
+        description: "Unique identifier of the configuration entry"
+      - field: key
+        type: string
+        description: "Key of the configuration setting"
+      - field: value
+        type: string
+        description: "Stored value associated with the key"
+      - field: created
+        type: timestamp
+        description: "Timestamp when the configuration entry was created"
+*/
 SELECT id, key, value, created
 FROM configuration
 WHERE key=:key
