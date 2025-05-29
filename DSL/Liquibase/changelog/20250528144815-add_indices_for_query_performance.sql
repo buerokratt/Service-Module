a9d333fcc75a51e353959bd839488adad5e20478
Artsiom Beida <artsiom.beida@mindtitan.com>
2025-05-29 16:44:22 +0200

    changed indices migration

--- DSL/Liquibase/changelog/20250528144815-add_indices_for_query_performance.sql
+++ DSL/Liquibase/changelog/20250528144815-add_indices_for_query_performance.sql
@@ -4,3 +4,5 @@
 CREATE INDEX IF NOT EXISTS idx_services_service_id ON services(service_id);
 CREATE INDEX IF NOT EXISTS idx_services_current_state ON services(current_state) WHERE current_state = 'active';
 CREATE INDEX IF NOT EXISTS idx_services_is_common_deleted ON services(deleted, is_common) WHERE NOT deleted AND is_common;
+-- CREATE INDEX IF NOT EXISTS idx_services_name_trgm ON services USING gin (name gin_trgm_ops);
+CREATE INDEX IF NOT EXISTS idx_services_service_id_updated_at ON services (service_id, updated_at DESC);
