769b5053597917ab218d0eed386c89438f55ee24
1AhmedYasser <26207361+1AhmedYasser@users.noreply.github.com>
2025-06-05 04:08:26 +0300

    chore(478): Enhanced Pointer width and height

--- GUI/src/components/HeaderStepCounter/HeaderStep/index.tsx
+++ GUI/src/components/HeaderStepCounter/HeaderStep/index.tsx
@@ -10,7 +10,7 @@ type HeaderStepProps = {
 };
 
 const Pointer = ({ color = "#c4c5cb" }) => (
-  <svg width="19" height="38" viewBox="0 0 19 38" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <svg width="19.5" height="39" viewBox="0 0 19 38" fill="none" xmlns="http://www.w3.org/2000/svg">
     <path
       d="M18.2735 18.7735L0 0.5V37.5L18.2542 20.2066C18.6628 19.8195 18.6715 19.1715 18.2735 18.7735Z"
       fill={color}
