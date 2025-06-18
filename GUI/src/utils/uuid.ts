b707ba94b4f436c355e8d69aa41184329aa606ad
1AhmedYasser <26207361+1AhmedYasser@users.noreply.github.com>
2025-06-18 09:55:32 +0300

    chore(565): Enhanced UUID

--- GUI/src/utils/uuid.ts
+++ GUI/src/utils/uuid.ts
@@ -1,1 +1,5 @@
-export const uuid = (): string => new Date().getTime().toString(36) + Math.random().toString(36).slice(2);
+export function uuid() {
+  return "10000000-1000-4000-8000-100000000000".replace(/[018]/g, (c: any) =>
+    (c ^ (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))).toString(16)
+  );
+}
