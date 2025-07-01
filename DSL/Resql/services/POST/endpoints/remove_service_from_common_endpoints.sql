34cce5100c80137093dcbd45d3cbb067f8543cb1
Igor Krupenja <igor.krupenja@gmail.com>
2025-07-01 14:32:11 +0300

    PoC fix

---
+++ DSL/Resql/services/POST/endpoints/remove_service_from_common_endpoints.sql
@@ -0,0 +1,37 @@
+WITH latest_common_endpoints AS (
+    SELECT DISTINCT ON (endpoint_id) *
+    FROM endpoints
+    WHERE
+        service_ids @> ARRAY[:serviceId::uuid]
+        AND is_common = TRUE
+        AND deleted = FALSE
+    ORDER BY endpoint_id, id DESC
+)
+INSERT INTO endpoints (
+    endpoint_id,
+    service_ids,
+    name,
+    type,
+    file_name,
+    is_common,
+    definitions,
+    deleted,
+    created_at,
+    updated_at
+)
+SELECT
+    endpoint_id,
+    ARRAY(
+        SELECT unnest(service_ids) 
+        WHERE unnest(service_ids) != :serviceId::uuid
+    ) AS service_ids,
+    name,
+    type,
+    file_name,
+    is_common,
+    definitions,
+    deleted,
+    created_at,
+    updated_at
+FROM latest_common_endpoints
+WHERE array_length(service_ids, 1) > 1; 
