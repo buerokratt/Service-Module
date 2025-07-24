12d7f5a53841458083c968a2f95e8eb5ea52227d
Igor Krupenja <igor.krupenja@gmail.com>
2025-07-24 15:18:26 +0300

    fix(640): fix fe

--- DSL/Resql/services/POST/endpoints/remove_service_from_common_endpoints.sql
+++
@@ -1,6 +0,0 @@
--- This operation is no longer needed since we now have single service_id instead of array
--- When removing a service from a common endpoint, we would need to create a new endpoint
--- with a different service_id, but this logic needs to be rethought based on business requirements
--- For now, this query will not perform any operation
-SELECT NULL; 
--- todo igor check this
