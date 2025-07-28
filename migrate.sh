ec4a0dfce03001a92cc8bb845aa47e9eb7626834
Igor Krupenja <igor.krupenja@gmail.com>
2025-07-28 13:49:28 +0300

    fix(640): fix yaml path

--- migrate.sh
+++ migrate.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --rm --network bykstack -v $(pwd)/DSL/Liquibase:/liquibase/changelog liquibase/liquibase -v $(pwd)/DSL/Liquibase/changelog.yml:/liquibase/changelog.yml --defaultsFile=/liquibase/changelog/liquibase.properties --changelog-file=changelog.yml update
+docker run --rm --network bykstack -v $(pwd)/DSL/Liquibase:/liquibase/changelog liquibase/liquibase -v $(pwd)/DSL/Liquibase/changelog.yaml:/liquibase/changelog.yaml --defaultsFile=/liquibase/changelog/liquibase.properties --changelog-file=changelog.yaml update
