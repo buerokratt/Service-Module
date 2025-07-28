6ef11dbaac4658e228fef7c7aa770191edc58946
1AhmedYasser <26207361+1AhmedYasser@users.noreply.github.com>
2025-07-29 01:36:49 +0300

    chore(638): Removed Endpoints Disk Writes

--- GUI/src/services/service-builder.ts
+++ GUI/src/services/service-builder.ts
@@ -7,331 +7,29 @@ import {
   createEndpoint,
   createNewService,
   editService,
-  jsonToYml,
   testService,
   updateEndpoint,
 } from "resources/api-constants";
 import useServiceStore from "store/new-services.store";
 import useToastStore from "store/toasts.store";
-import { RawData, Step, StepType } from "types";
-import { EndpointData, EndpointDefinition, EndpointEnv, EndpointVariableData } from "types/endpoint";
+import { Step, StepType } from "types";
+import { EndpointData, EndpointVariableData } from "types/endpoint";
 import api from "../services/api-dev";
 import { NodeDataProps } from "types/service-flow";
 import { getLastDigits, removeTrailingUnderscores, toSnakeCase } from "utils/string-util";
 import { format } from "date-fns";
 import { AxiosError } from "axios";
 
-// refactor this file later
-
-const getEndpointVariables = (
-  key: string,
-  data?: {
-    variables: EndpointVariableData[];
-    rawData: RawData;
-  }
-): { [key: string]: any } => {
-  if (!data) return {};
-  const result: { [key: string]: any } = {};
-  data.variables.forEach((v) => {
-    if (["schema", "array"].includes(v.type)) {
-      if (v.type === "array" && v.arrayType !== "schema") {
-        if (v.value) result[v.name] = `\${[info.response.body.${key}["${v.name}"]]}`;
-        return;
-      }
-      const nestedResult: string[] = [];
-      getNestedVariables(v, key, v.name, nestedResult);
-      result[v.name] = `\${new Map([${nestedResult}])}`;
-      return;
-    }
-
-    if (v.value) {
-      if (v.value.startsWith("${")) {
-        result[v.name] = `\${incoming.params.${v.name}}`;
-      } else {
-        result[v.name] = `\${info.response.body.${key}["${v.name}"]}`;
-      }
-    }
-  });
-  if (Object.keys(result).length === 0) {
-    try {
-      [data.rawData.value, data.rawData.testValue].forEach((rawData) => {
-        if (!rawData) return;
-        const parsedData = JSON.parse(rawData);
-        Object.keys(parsedData).forEach((k) => {
-          if (typeof parsedData[k] === "object") {
-            const nestedResult: string[] = [];
-            getNestedRawData(parsedData[k], key, k, nestedResult);
-            result[k] = `\${new Map([${nestedResult}])}`;
-            return;
-          }
-          result[k] = `\${info.response.body.${key}["${k}"]}`;
-        });
-      });
-    } catch (e) {
-      console.log(e);
-    }
-  }
-  return result;
-};
-
-const getNestedVariables = (variable: EndpointVariableData, key: string, path: string, result: string[]) => {
-  const variableData = variable.type === "schema" ? variable.schemaData : variable.arrayData;
-  if (variableData instanceof Array) {
-    variableData.forEach((v) => {
-      if (["schema", "array"].includes(v.type)) {
-        getNestedVariables(v, key, `${path}.${v.name}`, result);
-        return;
-      }
-      result.push(`["${path}.${v.name}", info.response.body.${key}["${path}.${v.name}"]]`);
-    });
-  }
-};
-
-const getNestedRawData = (data: { [key: string]: any }, key: string, path: string, result: string[]) => {
-  Object.keys(data).forEach((k) => {
-    if (typeof data[k] === "object") {
-      getNestedRawData(data[k], key, `${path}.${k}`, result);
-      return;
-    }
-    result.push(`["${path}.${k}", info.response.body.${key}["${path}.${k}"]]`);
-  });
-};
-
-// Since we currently cannot mark variables as sensitive from GUI, we set all as sensitive
-const saveEndpointInfo = async (
-  selectedEndpoint: EndpointDefinition,
-  env: EndpointEnv,
-  endpointName: string,
-  endpoint: EndpointData
-) => {
-  await saveEndpointConfig(selectedEndpoint, env, endpointName, endpoint);
-
-  const steps = new Map();
-  steps.set("get-configs", {
-    call: "http.post",
-    args: {
-      url: `${import.meta.env.REACT_APP_API_URL}/services/endpoints/configs/${
-        endpoint.isCommon ? "common/" : ""
-      }${endpointName}-${env === EndpointEnv.Live ? "prod" : "test"}-configs`,
-      body: {
-        params: "${incoming.body.params}",
-        headers: "${incoming.body.headers}",
-        body: "${incoming.body.body}",
-      },
-    },
-    result: "configs",
-  });
-  steps.set("return_value", {
-    wrapper: false,
-    return: "${configs.response.body}",
-  });
-  const result = Object.fromEntries(steps.entries());
-
-  await api
-    .post(
-      jsonToYml(),
-      { result },
-      {
-        params: {
-          location: `${import.meta.env.REACT_APP_RUUTER_SERVICES_POST_PATH}/endpoints/info/${
-            endpoint.isCommon ? "common/" : ""
-          }${endpointName}-${env === EndpointEnv.Live ? "prod" : "test"}-info.yml`,
-        },
-      }
-    )
-    .then(console.log)
-    .catch(console.log);
-};
-
-const saveEndpointConfig = async (
-  endpoint: EndpointDefinition,
-  env: EndpointEnv,
-  endpointName: string,
-  data: EndpointData
-) => {
-  const headers = rawDataIfVariablesMissing(
-    endpoint,
-    "headers",
-    env,
-    assignEndpointVariables(env, "headers", endpoint.headers)
-  );
-  const body = rawDataIfVariablesMissing(endpoint, "body", env, assignEndpointVariables(env, "body", endpoint.body));
-  const params = rawDataIfVariablesMissing(
-    endpoint,
-    "params",
-    env,
-    assignEndpointVariables(env, "params", endpoint.params)
-  );
-  const filteredParams = Object.fromEntries(
-    Object.entries(params).filter(([_, value]) => !String(value).startsWith("${"))
-  );
-
-  const filteredBody = Object.fromEntries(Object.entries(body).filter(([_, value]) => !String(value).startsWith("${")));
-
-  const steps = new Map();
-  const variables: { [key: string]: string } = {};
-  assignValues(headers, "headers", variables);
-  assignValues(body ? filteredBody : body, "body", variables);
-  assignValues(params ? filteredParams : params, "params", variables);
-  steps.set("prepare_step", {
-    assign: variables,
-  });
-  const bodyStr = Object.keys(body ?? {}).map((b) => `["${b.replaceAll("__", ".")}", body_${b}]`);
-  steps.set("combine_step", {
-    assign: {
-      sensitive: `\${new Map([${
-        typeof headers === "string"
-          ? `["headers", headers]`
-          : `["headers", new Map([${Object.keys(headers ?? {}).map(
-              (h) => `["${h.replaceAll("__", ".")}", headers_${h}]`
-            )}])]`
-      }, ${typeof body === "string" ? `["body", body]` : `["body", new Map([${bodyStr}])]`}, ${
-        typeof params === "string"
-          ? `["params", params]`
-          : `["params", new Map([${Object.keys(filteredParams ?? {}).map(
-              (p) => `["${p.replaceAll("__", ".")}", params_${p}]`
-            )}])]`
-      }])}`,
-    },
-  });
-  steps.set("return_value", { wrapper: false, return: "${sensitive}" });
-  const result = Object.fromEntries(steps.entries());
-
-  await api
-    .post(
-      jsonToYml(),
-      { result },
-      {
-        params: {
-          location: `${import.meta.env.REACT_APP_RUUTER_SERVICES_POST_PATH}/endpoints/configs/${
-            data.isCommon ? "common/" : ""
-          }${endpointName}-${env === EndpointEnv.Live ? "prod" : "test"}-configs.yml`,
-        },
-      }
-    )
-    .then(console.log)
-    .catch(console.log);
-};
-
-const rawDataIfVariablesMissing = (
-  endpoint: EndpointDefinition,
-  key: "headers" | "body" | "params",
-  env: EndpointEnv,
-  data: { [key: string]: string }
-): { [key: string]: any } | string => {
-  if (Object.keys(data).length > 0) return data;
-  const rawData =
-    endpoint[key]?.rawData[env === EndpointEnv.Live ? "value" : "testValue"] ?? endpoint[key]?.rawData.value ?? "";
-  if (rawData === "") return "";
-  try {
-    assignNestedRawVariables(JSON.parse(rawData), key, "", data);
-    return data;
-  } catch (e) {
-    console.error(e);
-    return "";
-  }
-};
-const assignValues = (data: string | { [key: string]: string }, key: string, variables: { [key: string]: string }) => {
-  if (typeof data === "string") variables[key] = data;
-  else
-    Object.keys(data).forEach((v) => {
-      variables[`${key}_${v}`] = data[v];
-    });
-};
-
-const assignNestedRawVariables = (
-  data: { [key: string]: any },
-  key: string,
-  path: string,
-  result: { [key: string]: string }
-) => {
-  Object.keys(data).forEach((k) => {
-    if (typeof data[k] === "object") {
-      return assignNestedRawVariables(data[k], key, path.length === 0 ? k : `${path}__${k}`, result);
-    }
-    result[path.length > 0 ? `${path}__${k}` : k] =
-      typeof data[k] === "string" && data[k].startsWith("{{")
-        ? data[k].replace("{{", `\${incoming.body.${key}["`).replace("}}", `"]}`)
-        : data[k];
-  });
-};
-
-const assignNestedVariable = (
-  variable: EndpointVariableData,
-  key: string,
-  env: EndpointEnv,
-  path: string,
-  result: { [key: string]: string }
-) => {
-  const variableData = variable.type === "schema" ? variable.schemaData : variable.arrayData;
-  if (variableData instanceof Array) {
-    variableData.forEach((v) => {
-      if (["schema", "array"].includes(v.type)) assignNestedVariable(v, key, env, `${path}__${v.name}`, result);
-      if (!v.value) return;
-
-      result[`${path}__${v.name}`] = (env === EndpointEnv.Test && v.testValue ? v.testValue : v.value)
-        .replace("{{", `\${incoming.body.${key}["`)
-        .replace("}}", `"]}`);
-    });
-  }
-};
-
-const assignEndpointVariables = (
-  env: EndpointEnv,
-  key: string,
-  data?: { variables: EndpointVariableData[]; rawData: RawData }
-): { [key: string]: string } => {
-  if (!data) return {};
-  const result: { [key: string]: string } = {};
-  data.variables.forEach((v) => {
-    if (!v.value) {
-      assignNestedVariable(v, key, env, v.name, result);
-      return;
-    }
-    // if test value is missing use live value
-    result[v.name] = (env === EndpointEnv.Test && v.testValue ? v.testValue : v.value)
-      .replace("{{", `\${incoming.body.${key}["`)
-      .replace("}}", `"]}`);
-  });
-  return result;
-};
-
 export async function saveEndpoints(endpoints: EndpointData[], onSuccess?: () => void, onError?: (e: any) => void) {
   const tasks: Promise<any>[] = [];
   const serviceId = useServiceStore.getState().serviceId;
-  const serviceName = removeTrailingUnderscores(useServiceStore.getState().serviceNameDashed());
+
 
   for (const endpoint of endpoints) {
     const selectedEndpointType = endpoint.definitions.find((e) => e.isSelected);
     if (!selectedEndpointType) continue;
-
-    const endpointName = `${serviceName.replaceAll(" ", "_")}-${getEndpointName(endpoint)}`;
-    endpoint.fileName = endpointName;
-    for (const env of [EndpointEnv.Live, EndpointEnv.Test]) {
-      await saveEndpointInfo(selectedEndpointType, env, endpointName, endpoint);
-    }
-    const steps = buildSteps(endpointName, endpoint, selectedEndpointType);
-    const result = Object.fromEntries(steps.entries());
-
     endpoint.serviceId = serviceId;
     endpoint.isCommon = endpoint.isCommon ?? false;
-    const isCommonPath = endpoint.isCommon ? "common/" : "";
-
-    tasks.push(
-      api.post(
-        jsonToYml(),
-        { result },
-        {
-          params: {
-            location: `${
-              import.meta.env.REACT_APP_RUUTER_SERVICES_PATH
-            }/${selectedEndpointType.methodType.toUpperCase()}/${
-              import.meta.env.REACT_APP_RUUTER_SERVICES_DIR_PATH
-            }/endpoints/${isCommonPath}${endpointName}.yml`,
-          },
-        }
-      )
-    );
   }
 
   endpoints.forEach((endpoint) => {
@@ -360,91 +58,6 @@ async function createEndpointAndUpdateState(endpoint: EndpointData): Promise<any
   endpoint.isNew = false;
   return response;
 }
-
-const buildSteps = (endpointName: string, endpoint: EndpointData, selectedEndpointType: EndpointDefinition) => {
-  const steps = new Map();
-  steps.set("extract_request_data", {
-    assign: {
-      type: "${incoming.params.type}",
-    },
-    next: "check_for_type",
-  });
-  steps.set("check_for_type", {
-    switch: [{ condition: "${type == null}", next: "return_no_type_error" }],
-    next: "check_for_environment",
-  });
-  steps.set("check_for_environment", {
-    switch: [{ condition: "${type.toLowerCase() == 'prod'}", next: "get_prod_info" }],
-    next: "get_test_info",
-  });
-  steps.set("get_prod_info", {
-    call: "http.post",
-    args: {
-      url: `${import.meta.env.REACT_APP_API_URL}/services/endpoints/info/${
-        endpoint.isCommon ? "common/" : ""
-      }${endpointName}-prod-info`,
-      body: {
-        params: "${incoming.body != null ? incoming.body.params ?? new Map() : new Map()}",
-        headers: "${incoming.body != null ? incoming.body.headers ?? new Map() : new Map()}",
-        body: "${incoming.body != null ? incoming.body.body ?? new Map() : new Map()}",
-      },
-    },
-    result: "info",
-    next: "assign_endpoint_url",
-  });
-  steps.set("get_test_info", {
-    call: `http.post`,
-    args: {
-      url: `${import.meta.env.REACT_APP_API_URL}/services/endpoints/info/${
-        endpoint.isCommon ? "common/" : ""
-      }${endpointName}-test-info`,
-      body: {
-        params: "${incoming.body != null ? incoming.body.params ?? new Map() : new Map()}",
-        headers: "${incoming.body != null ? incoming.body.headers ?? new Map() : new Map()}",
-        body: "${incoming.body != null ? incoming.body.body ?? new Map() : new Map()}",
-      },
-    },
-    result: "info",
-    next: "assign_endpoint_url",
-  });
-  const endpointParams = getEndpointVariables("params", selectedEndpointType.params);
-  const endpointHeaders = getEndpointVariables("headers", selectedEndpointType.headers);
-  const endpointBody = getEndpointVariables("body", selectedEndpointType.body);
-  const headers = Object.keys(endpointHeaders).length > 0 ? endpointHeaders : undefined;
-  const params = Object.keys(endpointParams).length > 0 ? endpointParams : undefined;
-  const body = Object.keys(endpointBody).length > 0 ? endpointBody : undefined;
-
-  let endpointUrl = selectedEndpointType.url?.split("?")[0];
-  steps.set("assign_endpoint_url", {
-    assign: {
-      endpoint_url: endpointUrl,
-    },
-    next: "execute_endpoint",
-  });
-  steps.set("execute_endpoint", {
-    call: selectedEndpointType.methodType.toLowerCase() === "get" ? "http.get" : "http.post",
-    args: {
-      url: "${endpoint_url}",
-      query: params,
-      headers,
-      body,
-    },
-    result: "res",
-    next: "return_result",
-  });
-  steps.set("return_result", {
-    wrapper: false,
-    return: "${res.response.body}",
-    next: "end",
-  });
-  steps.set("return_no_type_error", {
-    status: "400",
-    return: "Please Specify Endpoint Type 'prod' Or 'test'",
-    next: "end",
-  });
-
-  return steps;
-};
 
 interface SaveFlowConfig {
   steps: Step[];
@@ -725,11 +338,11 @@ function getYamlContent(nodes: Node[], edges: Edge[], steps: Step[], name: strin
       const parentStepName = toSnakeCase(parentNode.data.label);
 
       if (parentNode.data.stepType === StepType.Textfield) {
-        return handleTextField(finishedFlow, parentStepName, parentNode, childNode, childNodeId);
+        return handleTextField(finishedFlow, parentStepName, parentNode, childNode);
       }
 
       if (parentNode.data.stepType === StepType.Assign) {
-        return handleAssignStep(parentNode, finishedFlow, parentStepName, childNode, childNodeId);
+        return handleAssignStep(parentNode, finishedFlow, parentStepName, childNode);
       }
 
       if (parentNode.data.stepType === StepType.Condition) {
@@ -741,7 +354,7 @@ function getYamlContent(nodes: Node[], edges: Edge[], steps: Step[], name: strin
       }
 
       if (parentNode.data.stepType === StepType.MultiChoiceQuestion) {
-        return handleMultiChoiceQuestion(finishedFlow, parentStepName, parentNode, childNode, childNodeId);
+        return handleMultiChoiceQuestion(finishedFlow, parentStepName, parentNode, childNode);
       }
 
       if (parentNode.data.stepType === StepType.UserDefined) {
@@ -826,7 +439,6 @@ function handleTextField(
   parentStepName: string,
   parentNode: Node,
   childNode: Node<NodeDataProps> | undefined,
-  childNodeId: any
 ) {
   const htmlToMarkdown = new NodeHtmlMarkdown({
     textReplace: [
@@ -888,7 +500,6 @@ function handleAssignStep(
   finishedFlow: Map<any, any>,
   parentStepName: string,
   childNode: Node<NodeDataProps> | undefined,
-  childNodeId: any
 ) {
   const invalidElementsExist = hasInvalidElements(parentNode.data.assignElements ?? []);
   const isInvalid =
@@ -958,7 +569,6 @@ function handleMultiChoiceQuestion(
   parentStepName: string,
   parentNode: Node<NodeDataProps>,
   childNode: Node<NodeDataProps> | undefined,
-  childNodeId: any
 ) {
   return finishedFlow.set(parentStepName, {
     assign: {
@@ -1075,10 +685,6 @@ const getTemplateDataFromNode = (node: Node): { templateName: string; body?: any
       },
     };
   }
-};
-
-const getEndpointName = (endpoint: EndpointData) => {
-  return `${(endpoint.name.trim().length ?? 0) > 0 ? endpoint?.name.replaceAll(" ", "_") : endpoint?.endpointId}`;
 };
 
 export const saveFlowClick = async (status: "draft" | "ready" = "ready") => {
