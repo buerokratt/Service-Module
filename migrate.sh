6dc3beee8b17cd7f6e437ee257f580b58bb151c2
Igor Krupenja <igor.krupenja@gmail.com>
2025-07-28 14:01:22 +0300

    fix(562): fix the bug

--- migrate.sh
+++ migrate.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-docker run --rm --network bykstack -v $(pwd)/DSL/Liquibase:/liquibase/changelog liquibase/liquibase -v $(pwd)/DSL/Liquibase/changelog.yaml:/liquibase/changelog.yaml --defaultsFile=/liquibase/changelog/liquibase.properties --changelog-file=changelog.yaml update
+docker run --rm --network bykstack \
+  -v $(pwd)/DSL/Liquibase:/workspace \
+  -w /workspace \
+  liquibase/liquibase \
+  --defaultsFile=liquibase.properties \
+  --changelog-file=changelog.yaml \
+  update
