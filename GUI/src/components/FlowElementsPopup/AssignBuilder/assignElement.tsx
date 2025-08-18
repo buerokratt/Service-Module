18486911ba5fd97270b7be4c2e4dd5129245c8bb
Igor Krupenja <igor.krupenja@gmail.com>
2025-08-18 08:25:06 +0300

    fix(665): WIP toggle

--- GUI/src/components/FlowElementsPopup/AssignBuilder/assignElement.tsx
+++ GUI/src/components/FlowElementsPopup/AssignBuilder/assignElement.tsx
@@ -140,8 +140,12 @@ const AssignElement: React.FC<AssignElementProps> = ({
           <button
             className="small-assign-button assign-blue"
             onClick={() => {
-              setIsObjectEditorOpen(!isObjectEditorOpen);
-              if (isObjectEditorOpen) setIsEditingManually(true);
+              if (isObjectEditorOpen) {
+                setIsObjectEditorOpen(!isObjectEditorOpen);
+                setIsEditingManually(true);
+              } else {
+                setIsObjectEditorOpen(!isObjectEditorOpen);
+              }
             }}
           >
             <Icon icon={<MdDataObject />} />
