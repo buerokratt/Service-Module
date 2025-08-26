f7d0f569701f9ec384091f79d80087bd6264c076
Igor Krupenja <igor.krupenja@gmail.com>
2025-08-26 17:37:48 +0300

    fix(679): Fix Vite config

--- GUI/vite.config.ts
+++ GUI/vite.config.ts
@@ -1,6 +1,7 @@
 import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
+
 import react from '@vitejs/plugin-react';
+import { defineConfig, loadEnv } from 'vite';
 import svgr from 'vite-plugin-svgr';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
