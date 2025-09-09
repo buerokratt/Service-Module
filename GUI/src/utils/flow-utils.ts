b8d788c209bd8e6c559a193be12248bb69482426
Igor Krupenja <igor.krupenja@gmail.com>
2025-09-09 11:41:07 +0300

    fix(727): To do

--- GUI/src/utils/flow-utils.ts
+++ GUI/src/utils/flow-utils.ts
@@ -34,6 +34,9 @@ export const getNodeLabel = (step: Step, nodes: Node[]) => {
 export const isStepInvalid = (node: NodeDataProps): boolean => {
   if (node.testingPassed === false) return true;
 
+  console.log('node', node.stepType, node.name);
+
+  // todo condition
   if (node.stepType === StepType.Input || node.stepType === StepType.Condition) {
     const hasInvalidRules = (elements: any[]): boolean => {
       return elements.some((e) => {
@@ -52,6 +55,7 @@ export const isStepInvalid = (node: NodeDataProps): boolean => {
     return node.rules?.children === undefined || invalidRulesExist || node.rules?.children.length === 0;
   }
 
+  // todo multi
   if (node.stepType === StepType.MultiChoiceQuestion) {
     return (
       !node?.multiChoiceQuestion?.question ||
@@ -59,6 +63,7 @@ export const isStepInvalid = (node: NodeDataProps): boolean => {
     );
   }
 
+  // todo dynamic choices
   if (node.stepType === StepType.DynamicChoices) {
     return !node?.dynamicChoices?.list || !node?.dynamicChoices?.serviceName || !node?.dynamicChoices?.key;
   }
@@ -67,6 +72,7 @@ export const isStepInvalid = (node: NodeDataProps): boolean => {
   if (node.stepType === StepType.OpenWebpage) return !node.link || !node.linkText;
   if (node.stepType === StepType.FileGenerate) return !node.fileName || !node.fileContent;
   if (node.stepType === StepType.FileSign) return !node.signOption;
+  // todo assign
   if (node.stepType === StepType.Assign) {
     const hasInvalidElements = (elements: any[]): boolean => {
       return elements.some((e) => {
@@ -79,5 +85,6 @@ export const isStepInvalid = (node: NodeDataProps): boolean => {
     return node?.assignElements === undefined || invalidElementsExist || node?.assignElements.length === 0;
   }
 
+  // todo message length
   return !node.readonly && !node.message?.length;
 };
