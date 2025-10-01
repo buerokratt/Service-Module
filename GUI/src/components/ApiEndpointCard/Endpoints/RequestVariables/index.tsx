9a802c95ebdfc22b29bfa6ce33c5b2e9c09cb1e7
Igor Krupenja <igor.krupenja@gmail.com>
2025-10-01 16:11:54 +0300

    fix(702): wip variables

--- GUI/src/components/ApiEndpointCard/Endpoints/RequestVariables/index.tsx
+++ GUI/src/components/ApiEndpointCard/Endpoints/RequestVariables/index.tsx
@@ -279,6 +279,9 @@ const RequestVariables: React.FC<RequestVariablesProps> = ({
         isLive,
         getTabsRowsData,
       }),
+    // Adding missing dependencies breaks focus on typing in variable inputs
+    // Impossible to fix without a significant refactor
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     [deletedVariable],
   );
 
