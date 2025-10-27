8d5c343736893f0e2cac3149a0564b4587648900
exiled <vassilimoskaljov@gmail.com>
2025-10-27 10:27:32 +0200

    Moved logout logic to new hook

--- GUI/src/App.tsx
+++ GUI/src/App.tsx
@@ -8,8 +8,7 @@ import { ToastProvider } from './components/Toast/ToastProvider';
 import RootComponent from './RootComponent';
 import useStore from './store/store';
 import { UserInfo } from './types/userInfo';
-import {CHAT_SESSIONS} from "./constants/consts";
-import {generateUEID} from "./utils/generateUEID";
+import useTabCloseEffect from "./hooks/useTabCloseEffects";
 
 const App: React.FC = () => {
   useQuery<{
@@ -31,68 +30,7 @@ const App: React.FC = () => {
     enabled: import.meta.env.REACT_APP_LOCAL !== 'true',
   });
 
-  useEffect(() => {
-    const delay = 1000;
-
-    const timeOutId = setTimeout(() => {
-      initializeSession();
-    }, delay);
-
-    return () => clearTimeout(timeOutId);
-  }, []);
-
-
-  const initializeSession = () => {
-    let tabId = sessionStorage.getItem(CHAT_SESSIONS.SESSION_ID_KEY);
-    if (!tabId) {
-      tabId = generateUEID();
-      sessionStorage.setItem(CHAT_SESSIONS.SESSION_ID_KEY, tabId);
-    }
-
-    let currentState = getCurrentSessionState();
-
-    if (!currentState.ids.includes(tabId)) {
-      currentState.ids.push(tabId);
-      currentState.count = currentState.ids.length;
-      localStorage.setItem(
-          CHAT_SESSIONS.SESSION_STATE_KEY,
-          JSON.stringify(currentState)
-      );
-    }
-
-    const handleTabClose = () => {
-      const currentAppState = JSON.parse(
-          localStorage.getItem(CHAT_SESSIONS.SESSION_STATE_KEY) as string
-      ) || { ids: [], count: 0 };
-
-      const updatedIds = currentAppState.ids.filter(
-          (id: string) => id !== tabId
-      );
-      const updatedState = {
-        ids: updatedIds,
-        count: updatedIds.length,
-      };
-
-      localStorage.setItem(
-          CHAT_SESSIONS.SESSION_STATE_KEY,
-          JSON.stringify(updatedState)
-      );
-    };
-
-    window.addEventListener("beforeunload", handleTabClose);
-
-    return () => {
-      window.removeEventListener("beforeunload", handleTabClose);
-    };
-  };
-
-  const getCurrentSessionState = () => {
-    return (
-        JSON.parse(
-            localStorage.getItem(CHAT_SESSIONS.SESSION_STATE_KEY) as string
-        ) || { ids: [], count: 0 }
-    );
-  };
+  useTabCloseEffect();
 
   return (
     <BrowserRouter basename={import.meta.env.BASE_URL}>
