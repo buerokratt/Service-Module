fb1eb6f06007ec894602c7e619098e7a91068000
exiled <vassilimoskaljov@gmail.com>
2025-10-31 01:04:19 +0200

    Refactored newHeader and added predefined values to external scss

--- GUI/src/components/NewServiceHeader/index.tsx
+++ GUI/src/components/NewServiceHeader/index.tsx
@@ -1,7 +1,6 @@
 import { t } from 'i18next';
-import React, { FC, useState } from 'react';
+import React, {FC, useState} from 'react';
 import useServiceStore from 'store/new-services.store';
-import '@buerokratt-ria/header/src/Header.scss';
 import { useNavigate, useParams } from 'react-router-dom';
 import { deleteService } from 'resources/api-constants';
 import { ROUTES } from 'resources/routes-constants';
@@ -12,6 +11,7 @@ import { removeTrailingUnderscores } from 'utils/string-util';
 import { Button, HeaderStepCounter, Modal, Track } from '..';
 import api from '../../services/api-dev';
 import useServiceListStore from '../../store/services.store';
+import './NewServiceHeader.scss';
 
 type NewServiceHeaderProps = {
   activeStep: number;
@@ -33,10 +33,10 @@ const NewServiceHeader: FC<NewServiceHeaderProps> = ({ activeStep, backOnClick, 
 
   return (
     <>
-      <header className="header" style={{ paddingLeft: 24 }}>
+      <header className="header">
         <Track justify="between" gap={16}>
           <Button appearance="text" style={{ textDecoration: 'none', boxShadow: 'none' }} onClick={backOnClick}>
-            <h1 style={{ whiteSpace: 'nowrap', color: 'black' }}>{`< ${t('menu.backToServiceListing')}`}</h1>
+            <h1>{`< ${t('menu.backToServiceListing')}`}</h1>
           </Button>
           <HeaderStepCounter activeStep={activeStep} />
           <Button
