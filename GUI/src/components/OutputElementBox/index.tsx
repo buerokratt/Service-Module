147a943ae511c3863ff5d3e8fb5cd6f7a1ec5066
1AhmedYasser <26207361+1AhmedYasser@users.noreply.github.com>
2025-11-23 23:13:17 +0200

    chore(816): Fixed Format/Lint

--- GUI/src/components/OutputElementBox/index.tsx
+++ GUI/src/components/OutputElementBox/index.tsx
@@ -15,56 +15,52 @@ type OutputElementBoxProps = {
   readonly isAssignElement?: boolean;
 };
 
-const OutputElementBox = forwardRef<HTMLDivElement, OutputElementBoxProps>(({
-  borderColor,
-  dragData,
-  onClick,
-  style,
-  className,
-  children,
-  isAssignElement = false,
-}, ref) => {
-  const node = useServiceStore((state) => state.selectedNode);
-  const mergedStyle: CSSProperties = {
-    borderRadius: 46,
-    paddingTop: 1.5,
-    paddingBottom: 1.5,
-    paddingLeft: 10,
-    paddingRight: 10,
-    fontSize: 14,
-    ...style,
-    ...(borderColor && { border: `2px outset ${borderColor}` }),
-  };
+const OutputElementBox = forwardRef<HTMLDivElement, OutputElementBoxProps>(
+  ({ borderColor, dragData, onClick, style, className, children, isAssignElement = false }, ref) => {
+    const node = useServiceStore((state) => state.selectedNode);
+    const mergedStyle: CSSProperties = {
+      borderRadius: 46,
+      paddingTop: 1.5,
+      paddingBottom: 1.5,
+      paddingLeft: 10,
+      paddingRight: 10,
+      fontSize: 14,
+      ...style,
+      ...(borderColor && { border: `2px outset ${borderColor}` }),
+    };
 
-  const handleDragStart = (event: DragEvent<HTMLDivElement>) => {
-    if (!dragData) return;
+    const handleDragStart = (event: DragEvent<HTMLDivElement>) => {
+      if (!dragData) return;
 
-    const dragValue = isAssignElement ? `\${${dragData.key}}` : dragData.value;
+      const dragValue = isAssignElement ? `\${${dragData.key}}` : dragData.value;
 
-    event.dataTransfer.setData(
-      ASSIGN_DRAG_TYPE,
-      // Need to check for StepType.Assign here since ReactQuill does not support custom onDrop events
-      node?.data.stepType === StepType.Assign ||
-        node?.data.stepType === StepType.DynamicChoices ||
-        node?.data.stepType === StepType.Condition
-        ? JSON.stringify(dragData)
-        : dragValue,
+      event.dataTransfer.setData(
+        ASSIGN_DRAG_TYPE,
+        // Need to check for StepType.Assign here since ReactQuill does not support custom onDrop events
+        node?.data.stepType === StepType.Assign ||
+          node?.data.stepType === StepType.DynamicChoices ||
+          node?.data.stepType === StepType.Condition
+          ? JSON.stringify(dragData)
+          : dragValue,
+      );
+    };
+
+    return (
+      <Box
+        ref={ref}
+        className={className}
+        onClick={onClick}
+        color="green"
+        draggable={!!dragData}
+        onDragStart={handleDragStart}
+        style={mergedStyle}
+      >
+        {children}
+      </Box>
     );
-  };
+  },
+);
 
-  return (
-    <Box
-      ref={ref}
-      className={className}
-      onClick={onClick}
-      color="green"
-      draggable={!!dragData}
-      onDragStart={handleDragStart}
-      style={mergedStyle}
-    >
-      {children}
-    </Box>
-  );
-});
+OutputElementBox.displayName = 'OutputElementBox';
 
 export default OutputElementBox;
