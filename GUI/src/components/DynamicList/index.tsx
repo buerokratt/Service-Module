13373878b81b14494f15fdf3e44b05264c7aecaf
exiled <vassilimoskaljov@gmail.com>
2025-11-25 09:41:33 +0200

    linter checks

--- GUI/src/components/DynamicList/index.tsx
+++ GUI/src/components/DynamicList/index.tsx
@@ -1,13 +1,10 @@
 import { FC } from 'react';
-
 import { useTranslation } from 'react-i18next';
-
 import { LuMinus, LuPlus } from 'react-icons/lu';
 
 import { FormInput, Icon } from '../index';
 
 import './DynamicList.scss';
-
 import { InfoTooltip } from '../InfoTooltip';
 
 type DynamicListProps = {
