9d916604851c090bc24d03eff4d3a309da0d6200
exiled <vassilimoskaljov@gmail.com>
2025-11-25 09:36:48 +0200

    linter checks

--- GUI/src/components/InfoTooltip/index.tsx
+++ GUI/src/components/InfoTooltip/index.tsx
@@ -1,26 +1,28 @@
+import React from 'react';
 import { Tooltip } from 'components';
 import { useTranslation } from 'react-i18next';
+
 import { AiOutlineInfoCircle } from 'react-icons/ai';
 import Icon from '../Icon';
 
 interface TooltipProps {
-    name: string;
+  name: string;
 }
 
 export const InfoTooltip: React.FC<TooltipProps> = ({ name }) => {
-    const { t } = useTranslation();
+  const { t } = useTranslation();
 
-    return (
-        <div  style={{ display: "flex", alignItems: "center" }}>
-            <Tooltip content={t(name)}>
+  return (
+    <div style={{ display: 'flex', alignItems: 'center' }}>
+      <Tooltip content={t(name)}>
         <span>
           <Icon
-              style={{ paddingTop: '6px'}}
-              icon={<AiOutlineInfoCircle fontSize={20} color="#005aa3" />}
-              size="medium"
+            style={{ paddingTop: '6px' }}
+            icon={<AiOutlineInfoCircle fontSize={20} color="#005aa3" />}
+            size="medium"
           />
         </span>
-            </Tooltip>
-        </div>
-    );
+      </Tooltip>
+    </div>
+  );
 };
