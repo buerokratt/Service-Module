3253ec0f6c518cc4264272939e85d88b9040e3e4
Vassili Moskaljov <112167412+ExiRain@users.noreply.github.com>
2025-12-03 09:26:08 +0200

    819 move navigation (#821)
    * Adding new service modal.
    
    * Added header adjustments, moved map, adjusted canvas size, added back icon
    
    * Updated service header styles
    
    * Added new translations, moved logic of new modal to separate component, updated form input logic, created dynamic list component, updated state to hold new values
    
    * Added new translations, moved logic of new modal to separate component, updated form input logic, created dynamic list component, updated state to hold new values
    
    * Added keywords and examples to service dsl
    
    * Added new changeset, renamed keywords into entities, updated add dsl. added dmapper
    
    * Added entities and keywords to the flow
    
    * updated service flow page
    
    * Resolved conflicts
    
    * linter checks
    
    * linter checks
    
    * linter checks
    
    * linter checks
    
    * linter checks
    
    * linter checks
    
    * linter checks
    
    * linter checks
    
    * linter checks
    
    * Moved map
    
    * Updated styles and resolved conflicts
    
    * Linter fix

--- GUI/src/components/FlowBuilder/FlowBuilder.tsx
+++ GUI/src/components/FlowBuilder/FlowBuilder.tsx
@@ -18,6 +18,7 @@ import useNewServiceStore from 'store/new-services.store';
 import useServiceStore from 'store/services.store';
 import { StepType } from 'types';
 import '../Flow/LassoSelection/Lasso.css';
+import './FlowBuilder.scss';
 
 import { useThemeSyncWithFlow } from '../../hooks/useThemeSyncWithFlow';
 import HorizontalFlow from '../../static/icons/horizontal_flow.svg';
@@ -29,6 +30,7 @@ type FlowBuilderProps = {
 };
 
 const FlowBuilder: FC<FlowBuilderProps> = ({ nodes, edges }) => {
+  useLayout();
   const { getNodes, getEdges, setNodes, setEdges, getNode } = useReactFlow();
   const setReactFlowInstance = useNewServiceStore((state) => state.setReactFlowInstance);
   const [colorMode, setColorMode] = useState<ColorMode>('light');
@@ -55,7 +57,7 @@ const FlowBuilder: FC<FlowBuilderProps> = ({ nodes, edges }) => {
   const autoView = useServiceStore((state) => state.autoView);
   const toggleAutoView = useServiceStore((state) => state.toggleAutoView);
 
-  const { runLayout } = useLayout(orientation);
+  const { runLayout } = useLayout();
 
   const onConnect = useCallback(
     ({ source, target }: any) => {
@@ -188,9 +190,7 @@ const FlowBuilder: FC<FlowBuilderProps> = ({ nodes, edges }) => {
         defaultEdgeOptions={{ type: 'step', deletable: false }}
       >
         <Chat />
-        <div className={'controls-adjustment'}>
-          <MiniMap />
-        </div>
+        <MiniMap className={'minimap'} />
         <Background color="#D2D3D8" gap={16} lineWidth={9} />
         {isLassoActive && <Lasso />}
         <Panel position="top-left" style={zIndexStyle}>
@@ -218,18 +218,25 @@ const FlowBuilder: FC<FlowBuilderProps> = ({ nodes, edges }) => {
           </Track>
         </Panel>
         <Panel position="bottom-left">
-          <Track gap={10} direction="horizontal" align="center" style={{ paddingLeft: '110px', paddingBottom: '7px' }}>
-            <Controls orientation="horizontal" showInteractive={false} style={{ marginBottom: '12px' }} />
-            <Tooltip content={t('serviceFlow.autoFocus')}>
-              <span>
-                <Switch
-                  checked={autoView}
-                  onCheckedChange={toggleAutoView}
-                  onLabel={<Icon icon={<MdCenterFocusStrong fontSize={30} />} size="medium" />}
-                  offLabel={<Icon icon={<MdOutlineCenterFocusStrong fontSize={30} />} size="medium" />}
-                />
-              </span>
-            </Tooltip>
+          <Track gap={10} direction="horizontal" align="center" style={{}}>
+            <Controls
+              orientation="horizontal"
+              showInteractive={false}
+              style={{ marginLeft: '0' }}
+              className={'zoom-controls'}
+            />
+            <div className={'center-controls'}>
+              <Tooltip content={t('serviceFlow.autoFocus')}>
+                <span>
+                  <Switch
+                    checked={autoView}
+                    onCheckedChange={toggleAutoView}
+                    onLabel={<Icon icon={<MdCenterFocusStrong fontSize={30} />} size="medium" />}
+                    offLabel={<Icon icon={<MdOutlineCenterFocusStrong fontSize={30} />} size="medium" />}
+                  />
+                </span>
+              </Tooltip>
+            </div>
           </Track>
         </Panel>
       </ReactFlow>
