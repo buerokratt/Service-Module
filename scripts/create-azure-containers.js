7d81f754ddc95bf8db95ccfaa33d06cf7c7dd40f
Igor Krupenja <igor.krupenja@gmail.com>
2025-12-04 10:15:28 +0200

    feat(798): cm

--- scripts/create-azure-containers.js
+++ scripts/create-azure-containers.js
@@ -1,5 +1,5 @@
 /* eslint-disable */
-// This scriptreates required Azure blob storage containers in Azurite (local Azure Storage emulator)
+// This script creates required Azure blob storage containers in Azurite (local Azure Storage emulator)
 const { BlobServiceClient } = require('@azure/storage-blob');
 
 const containers = ['buerokratt']; // Add more container names as needed
