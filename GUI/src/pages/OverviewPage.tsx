aa4c6da18f06dd322895e553057e5d5b4e537139
exiled <vassilimoskaljov@gmail.com>
2025-12-16 18:27:44 +0200

    Remvoed intent connection from service module and related configurations

--- GUI/src/pages/OverviewPage.tsx
+++ GUI/src/pages/OverviewPage.tsx
@@ -5,7 +5,6 @@ import { useNavigate } from 'react-router-dom';
 
 import { Button, Track } from '../components';
 import ServicesTable from '../components/ServicesTable';
-import { trainingModuleTraining } from '../resources/api-constants';
 import { ROUTES } from '../resources/routes-constants';
 
 const OverviewPage: React.FC = () => {
@@ -23,10 +22,6 @@ const OverviewPage: React.FC = () => {
         <h1>{t('overview.commonServices')}</h1>
       </Track>
       <ServicesTable isCommon />
-      <p>
-        {t('overview.trainingModuleLink.text')}{' '}
-        <a href={trainingModuleTraining()}>{t('overview.trainingModuleLink.train')}</a>.
-      </p>
     </>
   );
 };
